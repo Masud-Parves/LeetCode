b51430b737a9075f679329d5fe5a4c2fb12e57ad
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-15 00:06:55 +0600

    Time: 196 ms (34.89%), Space: 43.6 MB (35.11%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    vector<pair<int, int>>adj[101];
-
-    void dijkstra(int src, vector<int>&dis){
+    void dijkstra(vector<vector<pair<int,int>>>& graph, int src, vector<int>&dis){
         priority_queue<pair<int, int>, vector<pair<int, int>>, 
         greater<pair<int, int>>> pq;
         
@@ -14,7 +12,7 @@ public:
             int currTime = pq.top().first;
             pq.pop();
             
-            for(auto edge : adj[currNode]){
+            for(auto edge : graph[currNode]){
                 int childNode = edge.second;
                 int childTime = edge.first;
                 
@@ -26,17 +24,18 @@ public:
         }
     }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> graph(n+1);
         for(auto time : times){
             int src = time[0];
             int des = time[1];
             int curTime = time[2];
-            adj[src].push_back({curTime , des});
+            graph[src].push_back({curTime , des});
         }
         
-        
+
         vector<int> dis(n+1, INT_MAX);
         
-        dijkstra(k, dis);
+        dijkstra(graph, k, dis);
         int answer = INT_MIN;
         for(int i=1; i<=n ; i++){
             answer = max(answer, dis[i]);

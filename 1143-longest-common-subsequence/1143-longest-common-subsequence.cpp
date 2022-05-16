698d5e378bd07a5025c5716b0b17b26e95a6dd76
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-17 00:44:19 +0600

    Time: 51 ms (16.09%), Space: 13 MB (51.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,7 +4,6 @@ public:
     int maxLen(int idx1, int idx2, string& s1, string& s2, vector<vector<int>>& DP){
         
         if(idx1 <0 || idx2<0) return 0;
-        if(idx1 == 0 && idx2 ==0) return s1[idx1]==s2[idx2];
         
         int &cache = DP[idx1][idx2];
         if(cache != -1){
@@ -23,10 +22,30 @@ public:
     }
     
     int longestCommonSubsequence(string text1, string text2) {
-        int len1 = text1.size(), len2 = text2.size();
+        int n = text1.size(), m = text2.size();
         
-        vector<vector<int>>DP(len1, vector<int>(len2, -1));
+        vector<vector<int>>DP(n+1, vector<int>(m+1, -1));
         
-        return maxLen(len1-1, len2-1, text1, text2, DP);
+        
+        for(int idx2 = 0; idx2<=m; idx2++){
+            DP[0][idx2] = 0;
+        }
+        
+        for(int idx1 = 0; idx1<=n; idx1++){
+            DP[idx1][0] = 0;
+        }
+        
+        
+        for(int idx1 = 1; idx1<=n ; idx1++){
+            for(int idx2 = 1; idx2<=m ; idx2++){
+                if(text1[idx1-1] == text2[idx2-1]){
+                    DP[idx1][idx2] = 1 + DP[idx1-1][idx2-1];
+                } else {
+                    DP[idx1][idx2] = max(DP[idx1-1][idx2], DP[idx1][idx2-1]);
+                }
+            }
+        }
+        
+        return DP[n][m];
     }
 };

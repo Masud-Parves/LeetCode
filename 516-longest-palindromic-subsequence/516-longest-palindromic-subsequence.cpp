364e3740ce2a41c948a3c345c01061547d72748f
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-20 01:01:34 +0600

    Time: 167 ms (40.91%), Space: 6.6 MB (98.54%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     
-    
-    int calculateLCS(int id1, int id2, string& s1, string& s2, vector<vector<int>>& DP){
-        if(id1< 0 || id2<0) return 0;
-
-        int &ret = DP[id1][id2];
-        if(ret != -1) return ret;
-        
-        if(s1[id1] == s2[id2]){
-            return 1 + calculateLCS(id1-1, id2-1, s1, s2, DP);
-        }
-        
-        return ret = max(calculateLCS(id1-1, id2, s1, s2, DP), calculateLCS(id1, id2-1, s1, s2, DP));
-    }
-    
     int getLCS(string& s1, string& s2){
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> DP (n+1, vector<int>(m+1, 0));
+        vector<int> previousRow(m+1, 0);
+        vector<int> currentRow(m+1, 0);
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 if(s1[i-1] == s2[j-1]){
-                    DP[i][j] = 1 + DP[i-1][j-1];
+                    currentRow[j] = 1 + previousRow[j-1];
                 } else {
-                    DP[i][j] = max(DP[i-1][j], DP[i][j-1]);
+                    currentRow[j] = max(previousRow[j], currentRow[j-1]);
                 }
             }
+            previousRow = currentRow;
         }
-        return DP[n][m];
+        return previousRow[m];
     }
     
     

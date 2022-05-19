db20871aa3280b512bbff15ac8a8bd9e08717f62
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-20 00:59:36 +0600

    Time: 230 ms (26.33%), Space: 73 MB (22.78%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -17,8 +17,18 @@ public:
     
     int getLCS(string& s1, string& s2){
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> DP (n, vector<int>(m, -1));
-        return calculateLCS(n-1, m-1, s1, s2, DP);
+        vector<vector<int>> DP (n+1, vector<int>(m+1, 0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1[i-1] == s2[j-1]){
+                    DP[i][j] = 1 + DP[i-1][j-1];
+                } else {
+                    DP[i][j] = max(DP[i-1][j], DP[i][j-1]);
+                }
+            }
+        }
+        return DP[n][m];
     }
     
     

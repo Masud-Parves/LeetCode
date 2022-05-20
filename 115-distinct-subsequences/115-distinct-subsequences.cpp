23fc958f080b7bb894c336bb46852e7e61469e2c
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-21 02:46:46 +0600

    Time: 58 ms (39.04%), Space: 6.5 MB (89.85%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
     
-    int calculateDistinct(int idx1, int idx2, string& s, string& t, vector<vector<int>>& DP){
-        
-        // Base case
-        if(idx2<0) return 1;
-        if(idx1<0) return 0;
-        
-        int &cache = DP[idx1][idx2];
-        if(cache != -1){
-            return cache;
-        }
-
-        cache = calculateDistinct(idx1-1, idx2, s, t, DP);
-        if(s[idx1] == t[idx2]) {
-            cache += calculateDistinct(idx1-1, idx2-1, s, t, DP);
-        }
-        return cache;
-    }
-    
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
         
-        vector<vector<double>> DP(n+1, vector<double>(m+1, 0));
+        vector<double> previousRow(m+1, 0);
+        vector<double> currentRow(m+1, 0);
         
-        for(int idx1 = 0; idx1<=n; idx1++){
-            DP[idx1][0] = 1;
-        }
+        previousRow[0] = currentRow[0] = 1;
 
         for(int i=1; i<=n ; i++){
             for(int j=1; j<=m; j++){
-                DP[i][j] = DP[i-1][j];
+                currentRow[j] = previousRow[j];
                 if(s[i-1] == t[j-1]){
-                    DP[i][j] += DP[i-1][j-1];
+                    currentRow[j] += previousRow[j-1];
                 }
             }
+            previousRow = currentRow;
         }
-
-        //int result = calculateDistinct(n-1, m-1, s, t, DP);
-        return (int)DP[n][m];
+        return (int)previousRow[m];
     }
 };

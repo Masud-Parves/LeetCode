609fcbeee5d5f0096984b73f0a6cbe784af5873e
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-21 02:52:39 +0600

    Time: 50 ms (50.85%), Space: 6.4 MB (89.85%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -5,20 +5,17 @@ public:
         int n = s.size();
         int m = t.size();
         
-        vector<double> previousRow(m+1, 0);
         vector<double> currentRow(m+1, 0);
-        
-        previousRow[0] = currentRow[0] = 1;
+        currentRow[0] = 1;
 
         for(int i=1; i<=n ; i++){
-            for(int j=1; j<=m; j++){
-                currentRow[j] = previousRow[j];
+            for(int j=m; j>=1; j--){
+                currentRow[j] = currentRow[j];
                 if(s[i-1] == t[j-1]){
-                    currentRow[j] += previousRow[j-1];
+                    currentRow[j] += currentRow[j-1];
                 }
             }
-            previousRow = currentRow;
         }
-        return (int)previousRow[m];
+        return (int)currentRow[m];
     }
 };

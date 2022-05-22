3d653535173e21a1e2fb2fc937e1ae2aa304685b
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-23 01:19:34 +0600

    Time: 173 ms (46.40%), Space: 93.3 MB (90.01%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -3,8 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int max_profit = 0, minValueTill = INT_MAX;
         for(auto& price : prices){
-            minValueTill = min(minValueTill, price);
-            max_profit = max(max_profit, price - minValueTill);
+            if(minValueTill > price)
+                minValueTill = price;
+            if(max_profit < price-minValueTill){
+                max_profit = price-minValueTill;
+            }
         }
         return max_profit;
     }

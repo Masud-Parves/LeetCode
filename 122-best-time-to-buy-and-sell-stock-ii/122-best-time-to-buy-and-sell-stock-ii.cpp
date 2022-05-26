296baa4f8272dbdacd7ccb2720e08847836932bc
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-26 23:29:52 +0600

    Time: 27 ms (5.04%), Space: 15.3 MB (8.99%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    
-    int helper(int idx, int buy, vector<int>& prices, vector<vector<int>>& DP){
-        
-        if(idx == prices.size()){
-            return 0;
-        }
-        
-        
-        int &cache = DP[idx][buy];
-        if(cache != -1) return cache;
-        
-        if(buy == 0){
-            int way1 = helper(idx+1, buy, prices, DP); // sell
-            int way2 = prices[idx] + helper(idx+1, 1-buy, prices, DP); //sell
-            cache = max(way1, way2);
-        } else {
-            int way1 = helper(idx+1, buy, prices, DP); // buy
-            int way2 = -prices[idx] + helper(idx+1, 1-buy, prices, DP); // not buy
-            cache = max(way1, way2);
-        }
-        return cache;
-    }
-    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>>DP(n+1, vector<int>(2, 0));
@@ -30,16 +7,16 @@ public:
         DP[n][0] = DP[n][1] = 0;
         
         for(int idx = n-1; idx>=0; idx--){
-            for(int buy=0; buy<=1; buy++){
-                
+            for(int buy = 0; buy<= 1 ; buy++){
                 if(buy == 1){
-                    DP[idx][buy] = max(-prices[idx] + DP[idx+1][1-buy], DP[idx+1][buy]);
+                    DP[idx][buy] = max(-prices[idx] + DP[idx+1][1-buy],
+                                      DP[idx+1][buy]);
                 } else {
-                    DP[idx][buy] = max(prices[idx] + DP[idx+1][1-buy], DP[idx+1][buy]);
+                    DP[idx][buy] = max(prices[idx] + DP[idx+1][1-buy],
+                                      DP[idx+1][buy]);
                 }
             }
         }
-        
         return DP[0][1];
     }
 };

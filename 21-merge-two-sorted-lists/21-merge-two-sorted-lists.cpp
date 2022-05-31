b437b16dca68f7c521e316f7163d68a7e9b15832
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-05-31 23:57:30 +0600

    Time: 7 ms (83.28%), Space: 14.6 MB (97.95%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -9,28 +9,23 @@
  * };
  */
 class Solution {
+private:
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        
+        if(l1 == NULL || l2 == NULL) {
+            return (l1 == NULL) ? l2 : l1;
+        }
+        
+        if(l1->val <= l2->val){
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-        if(list1 == NULL || list2 == NULL){
-            return (list1 == NULL) ? list2 : list1;
-        }
-        ListNode* dummy = new ListNode(-1);
-        ListNode* currNode = dummy;
-        while(list1 && list2){
-            if(list1->val <= list2->val){
-                currNode->next = new ListNode(list1->val);
-                list1 = list1->next;
-            } else {
-                currNode->next = new ListNode(list2->val);
-                list2 = list2->next;
-            }
-            currNode = currNode->next;
-        }
-        
-        if(list1) currNode->next = list1;
-        else if(list2) currNode->next = list2;
-        
-        return dummy->next;
+        return merge(list1, list2);
     }
 };

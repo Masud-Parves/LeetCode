aaac1ba2d858c816f2475b10dc1b440acd0130ca
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-06 23:13:46 +0600

    Time: 3 ms (90.31%), Space: 7.4 MB (33.01%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -10,19 +10,23 @@ private:
             return false;
         }
         
-        if(DP[i][j] != -1) return DP[i][j];
+        int &cache = DP[i][j];
+        if(cache != -1) {
+            return cache;
+        }
+        cache = 0;
         bool match = (i<s.size() && (s[i] == p[j] || p[j] == '.'));
         
         if(j+1<p.size() && p[j+1] == '*'){
-            bool pick = match ? isMatchExpression(i+1, j, s, p, DP) : false;
+            bool pick = match && isMatchExpression(i+1, j, s, p, DP);
             bool notPick = isMatchExpression(i, j+2, s, p, DP);
-            return DP[i][j] = pick || notPick;
+            return cache = pick || notPick;
         }
         
         if(match == true) {
-            return DP[i][j] = isMatchExpression(i+1, j+1, s, p, DP);
+            return cache = isMatchExpression(i+1, j+1, s, p, DP);
         }
-        return DP[i][j] = false;
+        return cache;
         
     }
     

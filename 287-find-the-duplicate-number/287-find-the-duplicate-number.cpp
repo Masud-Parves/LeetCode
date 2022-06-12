94894637c1af09abd0efb2972f6d1a8d343e7eba
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-12 22:49:25 +0600

    Time: 233 ms (23.48%), Space: 84.1 MB (13.01%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,25 +1,51 @@
 class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
+private:
+    int usingMissingTechnique(vector<int>& nums){
         int n = nums.size();
         
         int duplicate = -1;
-        
         for(int i=0; i<n; i++){
             int cur = abs(nums[i]);
-            
             if(nums[cur] < 0){
                 duplicate = cur;
                 break;
             }
-            
             nums[cur] *=-1;
         }
         
         for(auto num : nums){
             num = abs(num);
         }
-        
         return duplicate;
+    }
+    
+    int usingSortTechnique(vector<int> nums){
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        for(int i=1; i<n; i++){
+            if(nums[i]==nums[i-1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    
+    int usingHashMap(vector<int>& nums){
+        int n = nums.size();
+        unordered_map<int, int>seen;
+        
+        for(int i=0; i<n; i++){
+            if(seen.find(nums[i])!=seen.end()){
+                return nums[i];
+            }
+            seen[nums[i]] = i+1;
+        }
+        return -1;
+    }
+    
+public:
+    int findDuplicate(vector<int>& nums) {
+        return usingHashMap(nums);
     }
 };

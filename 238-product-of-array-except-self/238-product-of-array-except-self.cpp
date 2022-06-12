36f5bd1253ac439abdc0b806aee1d068b9625086
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-13 00:31:19 +0600

    Time: 52 ms (11.78%), Space: 25 MB (36.80%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,15 +3,21 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int>answer(n, 1);
+        vector<int>left(n, 1), right(n, 1);
         
         for(int i=1; i<n; i++) {
-            answer[i] = answer[i-1]*nums[i-1];
+            left[i] = left[i-1]*nums[i-1];
         }
-        int R = 1;
-        for(int i=n-1; i>=0; i--){
-            answer[i] *= R;
-            R *= nums[i];
+        
+        for(int i=n-2; i>=0; i--){
+            right[i] = right[i+1] * nums[i+1];
         }
+        
+        for(int i=0; i<n; i++){
+            answer[i] = left[i]*right[i];
+        }
+        
+        
         return answer;
     }
 };

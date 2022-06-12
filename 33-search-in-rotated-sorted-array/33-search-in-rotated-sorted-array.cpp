57b926ef3460ab08eb06537fd4ccb169e51a9d67
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-12 12:21:41 +0600

    Time: 9 ms (25.39%), Space: 11 MB (75.32%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,33 @@
 class Solution {
+private:
+    int helper(vector<int>& nums, int target, int L, int R){
+        int M = (L+R)/2;
+        
+        if(nums[M] == target) {
+            return M;
+        }
+        
+        if(L>R) return -1;
+        
+        if(nums[L]<=nums[M]){
+            if(nums[L]<=target && target<=nums[M]){
+                return helper(nums, target, L, M-1);
+            } else {
+                return helper(nums, target, M+1, R);
+            }
+        } else {
+            if(nums[M]<=target && target<=nums[R]){
+                return helper(nums, target, M+1, R);
+            }else {
+                return helper(nums, target, L, M-1);
+            }
+        }
+        return -1;
+    }
 public:
     int search(vector<int>& nums, int target) {
         int L = 0, R = nums.size()-1;
         
-        while(L<=R){
-            int M = (L+R)/2;
-            
-            if(nums[M] == target){
-                return M;
-            }
-            
-            if(nums[L] <= nums[M]){
-                // pivot not present in this segment
-                if(nums[L]<=target && target<=nums[M]){
-                    R = M - 1;
-                } else {
-                    L = M + 1;
-                }
-                
-            } else {
-                // pivot present in this segment
-                
-                if(nums[M]<=target && target<=nums[R]){
-                    L = M + 1;
-                } else {
-                    R = M - 1;
-                }
-            }
-            
-        }
-        return -1;
+        return helper(nums, target, L, R);
     }
 };

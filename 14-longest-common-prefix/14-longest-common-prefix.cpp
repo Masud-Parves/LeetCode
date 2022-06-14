9ade3afef9b41c8c0e0a7d2413649ec0340ab46f
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-15 00:19:56 +0600

    Time: 15 ms (9.66%), Space: 10.9 MB (9.45%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,24 +1,27 @@
 class Solution {
+private:
+    string mergedLCP(string& left, string& right){
+        int minLen = min(left.size(), right.size());
+        
+        for(int i=0; i<minLen; i++){
+            if(left[i] != right[i]){
+                return left.substr(0, i);
+            }
+        }
+        return left.substr(0, minLen);
+    }
+    string getLongestCommonPrefix(vector<string>& strs, int l, int r){
+        if( l == r){
+            return strs[l];
+        }
+        int mid = (l+r)/2;
+        string leftLCP = getLongestCommonPrefix(strs, l, mid);
+        string rightLCP = getLongestCommonPrefix(strs, mid+1, r);
+        return mergedLCP(leftLCP, rightLCP);
+    }
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        
-        string lcp = "";
-        for(int i=0; i<strs[0].size(); i++){
-            
-            bool allMatch = true;
-            for(int k = 1; k<n ; k++){
-                if(strs[0][i] != strs[k][i]){
-                    allMatch = false;
-                    break;
-                }
-            }
-            
-            if(allMatch == true) {
-                lcp += strs[0][i];
-            } else break;
-            
-        }
-        return lcp;
+        if(strs.size() == 0 || strs[0].size() == 0) return "";
+        return getLongestCommonPrefix(strs, 0, strs.size()-1);
     }
 };

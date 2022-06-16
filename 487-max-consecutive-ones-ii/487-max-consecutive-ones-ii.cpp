f8d7e405a9eda204929c37bfe89f2c541bc3f988
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-16 21:59:34 +0600

    Time: 42 ms (87.80%), Space: 36.2 MB (37.99%) - LeetHub

--- 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.cpp
+++ 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    // Time : O(n), Space : O(1), Technique : window sliding Technique
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int n = nums.size();
-        
-        int cntOne = 0, cntZero = 0, currWindow = 0, result = 0;
+        int cntZero = 0, currWindow = 0, result = 0;
         int l = 0, r = 0;
         while(r<n){
-            
             if(nums[r] == 0) {
                 cntZero++;
             }
-            
             currWindow++;
             while(cntZero>1) {
                 if(nums[l] == 0) cntZero--;
                 l++;
                 currWindow--;
             }
-            
             result = max(result, currWindow);
             r++;
         }

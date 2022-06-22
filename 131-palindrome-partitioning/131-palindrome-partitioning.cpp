94cd2354edf6da56f96ca8cc37d4af40e0fa7051
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-23 02:00:33 +0600

    Time: 111 ms (94.25%), Space: 49.3 MB (91.14%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,22 +1,30 @@
 // Topic : Backtrack
-// TC : O(n * 2^n), Hence, there could be 2^{N}2 
-/*N possible substrings in the worst case. For each substring, it takes \mathcal{O}(N)O(N) time to generate substring and determine if it a palindrome or not. This gives us time complexity as \mathcal{O}(N \cdot 2^{N})O(N⋅2 
-N)
- */
+// TC : O(2^n), where n is length of string.
 // SC : O(n) . where n is no of max partition. 
 
 class Solution {
-    void backtrack(int start, string& s, vector<vector<string>>& result, vector<string>& part, vector<vector<bool>>& DP){
-        if(start >= s.length()){
+    
+    bool isPali(string& s, int i, int j){
+        if(i==j) return true;
+        
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    void backtrack(int idx, string& s, vector<vector<string>>& result, vector<string>& part){
+        if(idx >= s.length()){
            result.push_back(part);
            return;
         } 
         
-        for(int end=start; end < s.length(); end++){
-            if(s[start] == s[end] && (end-start<=2 || DP[start+1][end-1] == true)){
-                DP[start][end] = true;
-                part.push_back(s.substr(start, end-start+1));
-                backtrack(end+1, s, result, part, DP);
+        for(int i=idx; i < s.length(); i++){
+            if(isPali(s, idx, i)){
+                part.push_back(s.substr(idx, i-idx+1));
+                backtrack(i+1, s, result, part);
                 part.pop_back();
             }
         }
@@ -24,12 +32,9 @@ class Solution {
     }
 public:
     vector<vector<string>> partition(string s) {
-        
-        int len = s.length();
         vector<vector<string>> result;
         vector<string> part;
-        vector<vector<bool>> DP(len, vector<bool>(len, false));
-        backtrack(0, s, result, part, DP);
+        backtrack(0, s, result, part);
         return result;
     }
 };

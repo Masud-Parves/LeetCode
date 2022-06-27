8742f9f66ad33c65789c07bf997ba8e03938f3ed
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-27 22:54:07 +0600

    Time: 39 ms (7.02%), Space: 21.1 MB (25.30%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -22,21 +22,39 @@ private:
         return 1 + max(leftDepth, rightDepth);
         
     }
+    
+    bool isTreeBalanced(TreeNode* node){
+        int leftHeight = depthOfBT(node->left);
+        int rightHeight = depthOfBT(node->right);
+        return (abs(leftHeight - rightHeight) <= 1);
+    }
 public:
     bool isBalanced(TreeNode* root) {
         if(root == NULL) {
             return true;
         }
         
-        int leftDepth = depthOfBT(root->left);
-        int rightDepth = depthOfBT(root->right);
+        queue<TreeNode*>Q;
+        Q.push(root);
         
-        if(abs(leftDepth - rightDepth)>1) {
-            return false;
+        while(!Q.empty()){
+            int size = Q.size();
+            for(int i=0; i<size; i++){
+                TreeNode* currNode = Q.front();
+                Q.pop();
+                
+                if(isTreeBalanced(currNode) == false){
+                    return false;
+                }
+                
+                if(currNode->left != NULL) {
+                    Q.push(currNode->left);
+                }
+                if(currNode->right != NULL){
+                    Q.push(currNode->right);
+                }
+            }
         }
-        
-        bool isLeftBalanced = isBalanced(root->left);
-        bool isRightBalanced = isBalanced(root->right);
-        return isLeftBalanced && isRightBalanced;
+        return true;
     }
 };

e0de3a5a429dc56f5a54c903d8ed61f44e4cad6f
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-28 20:53:32 +0600

    Time: 11 ms (7.12%), Space: 10 MB (44.09%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -1,28 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    
+    bool check(TreeNode* p, TreeNode* q){
+        if(p==NULL && q==NULL) return true;
+        //cout << p->val << " " << q->val << endl;
+        if(p == NULL or q == NULL or p->val != q->val) return false;
+        return true;
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == NULL && q == NULL) {
-            return true;
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL or p->val != q->val) return false;
+        
+        
+        queue < TreeNode* > P, Q;
+        P.push(p);
+        Q.push(q);
+        
+        while(!P.empty() && !Q.empty()){
+            TreeNode* up = P.front();
+            P.pop();
+            TreeNode* uq = Q.front();
+            Q.pop();
+            if(!check(up, uq)) return false;
+            if(up != NULL){
+
+                if(!check(up->left, uq->left)) return false;
+                //cout << up->left << " left " << uq->left << endl;
+                if(!check(up->right, uq->right)) return false;
+                //cout << up->right << " right " << uq->right << endl;
+                if(up->left && uq->left){
+                    P.push(up->left);
+                    Q.push(uq->left);
+                }
+                if(up->right && uq->right){
+                    P.push(up->right);
+                    Q.push(uq->right);
+                }
+            }
+            
         }
         
-        if(p == NULL || q == NULL || p->val != q->val) {
-            return false;
-        }
-        
-        bool isLeftTreeSame = isSameTree(p->left, q->left);
-        bool isRightTreeSame = isSameTree(p->right, q->right);
-        
-        return isLeftTreeSame && isRightTreeSame;
+        return true;
     }
 };

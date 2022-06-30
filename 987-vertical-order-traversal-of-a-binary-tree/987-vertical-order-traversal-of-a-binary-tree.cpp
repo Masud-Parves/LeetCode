42cfa498bc01f54c5e6aa887b96230c38c400e7c
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-06-30 08:02:07 +0600

    Time: 11 ms (36.15%), Space: 15.1 MB (9.07%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,45 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+private:
+    void preorderTraversal(TreeNode* node, int row, int col, map<int, map<int, multiset<int>>>& nodes){
+        if(node == NULL){
+            return;
+        }
+        
+        nodes[row][col].insert(node->val);
+        preorderTraversal(node->left, row-1, col+1, nodes);
+        preorderTraversal(node->right, row+1, col+1, nodes);
+    }
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> result;
         if(root == NULL) {
             return result;
         }
-        map<int, map<int, multiset<int>>> mp; // row, col, val
+        map<int, map<int, multiset<int>>> nodes; // row, col, val
         
-        queue<pair<TreeNode*, pair<int, int>>> Q;
-        Q.push({root, {0, 0}});
+        preorderTraversal(root, 0, 0, nodes);
         
-        while(!Q.empty()){
-            auto uNode = Q.front();
-            Q.pop();
-            TreeNode* node = uNode.first;
-            int row = uNode.second.first;
-            int col = uNode.second.second;
-            
-            mp[row][col].insert(node->val);
-            if(node->left != NULL) {
-                Q.push({node->left,{row-1, col+1}});
-            }
-            if(node->right != NULL) {
-                Q.push({node->right,{row+1, col+1}});
-            }
-        }
-        
-        for(auto& m : mp){
+        for(auto& node : nodes){
             vector<int>level;
-            for(auto ele : m.second){
+            for(auto ele : node.second){
                 level.insert(level.end(), ele.second.begin(), ele.second.end());
             }
             result.push_back(level);

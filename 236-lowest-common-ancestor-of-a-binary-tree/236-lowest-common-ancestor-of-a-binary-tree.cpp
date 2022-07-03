5f53db267d763d060fc4ff42a6b7772a3c20c066
Masud Parves <38131751+Masud-Parves@users.noreply.github.com>
2022-07-03 09:44:22 +0600

    Time: 19 ms (77.01%), Space: 14.3 MB (57.85%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,48 +8,21 @@
  * };
  */
 class Solution {
-private:
-    bool getPath(TreeNode* node, TreeNode* destNode, vector<int>& path){
-        if(node == NULL){
-            return false;
-        }
-        
-        path.push_back(node->val);
-        
-        if(node->val == destNode->val){
-            return true;
-        }
-        
-        if(getPath(node->left, destNode, path) || getPath(node->right, destNode, path)) {
-            return true;
-        }
-        
-        path.pop_back();
-        return false;
-    }
-    
-    TreeNode* getLCA(vector<int>& pathA, vector<int>& pathB){
-        int n = pathA.size();
-        int m = pathB.size();
-        TreeNode* lca = new TreeNode(-1);
-        for(int i=0; i<n && i<m; i++){
-            if(pathA[i] == pathB[i]){
-                lca->val = pathA[i];
-            } else break;
-        }
-        return lca;
-    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<int>pathA;
-        vector<int>pathB;
+        if(root == NULL || root == p || root == q){
+            return root;
+        }
         
-        getPath(root, p, pathA);
-        getPath(root, q, pathB);
-        for(auto x: pathA) cout << x <<" "; cout << endl;
-        for(auto x: pathB) cout << x << " ";
-        TreeNode* lca = getLCA(pathA, pathB);
+        TreeNode* leftSubtree =  lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightSubtree = lowestCommonAncestor(root->right, p, q);
         
-        return lca;
+        if(leftSubtree == NULL) {
+            return rightSubtree;
+        }else if(rightSubtree == NULL){
+            return leftSubtree;
+        } else {
+            return root;
+        }
     }
 };

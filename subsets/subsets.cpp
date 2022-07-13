d22e7959421fb2b1af8c800f8eb6db414c7cae1f
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-07-14 00:51:19 +0600

    Time: 8 ms, Memory: 16.1 MB - LeetHub

---
+++ subsets/subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void getSubset(int idx, vector<int>& nums, vector<int> subs, vector<vector<int>>& result){
+        if(idx == nums.size()){
+            result.push_back(subs);
+            return;
+        }
+        
+        getSubset(idx+1, nums, subs, result);
+        subs.push_back(nums[idx]);
+        getSubset(idx+1, nums, subs, result);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        if(nums.size() == 0) {
+            return result;
+        }
+        vector<int>subs;
+        getSubset(0, nums, subs, result);
+        return result;
+    }
+};

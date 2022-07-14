3c953a18b247e0c44e676745bb09ef5438bc8644
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-07-14 07:42:31 +0600

    Time: 7 ms (99.84%), Space: 26.4 MB (28.28%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,41 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 private:
-    
-    TreeNode* buildBinaryTree(int start, int end, int& take, vector<int>& preorder, unordered_map<int, int>& index){
-        if(start > end) {
+    TreeNode* construct(int start, int end, int& idx, vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& seen){
+        if(start>end){
             return NULL;
         }
         
-        TreeNode* node = new TreeNode(preorder[take++]);
-        int idx = index[node->val];
-        node->left = buildBinaryTree(start, idx-1, take, preorder, index);
-        node->right = buildBinaryTree(idx+1, end, take, preorder, index);
-        return node;
+        int rootVal = preorder[idx];
+        idx++;
+        TreeNode* root = new TreeNode(rootVal);
+        int getIdx = seen[rootVal];
+        
+        root->left = construct(start, getIdx-1, idx, preorder, inorder, seen);
+        root->right = construct(getIdx+1, end, idx, preorder, inorder, seen);
+        return root;
     }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         if(preorder.size() == 0) {
             return NULL;
         }
-        
-        // node value , idx
-        unordered_map<int, int> index;
-        for(int i=0; i<inorder.size(); i++){
-            index[inorder[i]] = i;
+        int n = inorder.size();
+        unordered_map<int, int>seen;
+        for(int i=0; i<n; i++){
+            seen[inorder[i]] = i;
         }
         int take = 0;
-        TreeNode* root = buildBinaryTree(0, inorder.size()-1, take, preorder, index);
+        TreeNode* root = construct(0, n-1, take, preorder, inorder, seen);
         return root;
     }
 };

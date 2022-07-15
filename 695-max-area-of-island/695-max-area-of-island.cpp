0d192de229e0f7e287aa6890294f152979956b02
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-07-15 21:22:29 +0600

    Time: 21 ms (84.06%), Space: 23.7 MB (42.38%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -2,28 +2,18 @@ class Solution {
 private:
     int fx[4] = {1, -1, 0, 0};
     int fy[4] = {0, 0, 1, -1};
-    int bfs(int r, int c, vector<vector<int>>& grid, vector<vector<bool>>& visited){
-        queue<pair<int, int>> Q;
-        Q.push({r, c});
-        visited[r][c] = true;
-        int ret = 0;
-        while(!Q.empty()){
-            pair<int, int> node = Q.front();
-            Q.pop();
-            ret++;
-            int ur = node.first;
-            int uc = node.second;
-            
-            for(int i=0; i<4; i++){
-                int vr = ur + fx[i];
-                int vc = uc + fy[i];
-                
-                if(vr>=0 && vr<grid.size() && vc>=0 && vc<grid[0].size() && visited[vr][vc] == false && grid[vr][vc] == 1){
-                    visited[vr][vc] = true;
-                    Q.push({vr, vc});
-                }
-            }
+    int dfs(int r, int c, vector<vector<int>>& grid, vector<vector<bool>>& visited){
+        if(r<0 or r==grid.size() or c<0 or c == grid[0].size() or grid[r][c] == 0 or visited[r][c] == true) {
+            return 0;
         }
+        
+        visited[r][c] = true;
+        int ret = 1;
+        
+        for(int i=0; i<4; i++){
+            ret += dfs(r+fx[i], c+fy[i], grid, visited);
+        }
+        
         return ret;
     }
 public:
@@ -35,7 +25,7 @@ public:
         for(int r=0; r<rows; r++){
             for(int c=0; c<cols; c++){
                 if(visited[r][c] == false && grid[r][c] == 1){
-                    int ret = bfs(r, c, grid, visited);
+                    int ret = dfs(r, c, grid, visited);
                     result = max(result, ret);
                 }
             }

cef3f247ef08efedce56c29c12057048522c62fc
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-07-17 00:47:12 +0600

    Time: 15 ms (15.89%), Space: 13.6 MB (87.87%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,30 +10,29 @@
  * };
  */
 class Solution {
-    TreeNode* constructBST(int& take, int L, int R, vector<int>& inorder, vector<int>& preorder, unordered_map<int, int>& seen){
-        if(L>R) return NULL;
+private:
+    TreeNode* constructBST(int& taken, int n, vector<int>& preorder, int lower, int upper){
+        if(taken == n){
+            return NULL;
+        }
         
-        int nodeVal = preorder[take++];
-        int pos = seen[nodeVal];
+        int nodeVal = preorder[taken];
+        
+        if(nodeVal<lower || nodeVal>upper){
+            return NULL;
+        }
+        taken++;
         TreeNode* root = new TreeNode(nodeVal);
-
-        root->left = constructBST(take, L, pos-1, inorder, preorder, seen);
-        root->right = constructBST(take, pos+1, R, inorder, preorder, seen);
         
+        root->left = constructBST(taken, n, preorder, lower, root->val);
+        root->right = constructBST(taken, n, preorder, root->val, upper);
         return root;
     }
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int n = preorder.size();
-        vector<int>inorder(preorder.begin(), preorder.end());
-        sort(inorder.begin(), inorder.end());
-        
-        unordered_map<int, int> seen;
-        for(int i=0; i<inorder.size(); i++){
-            seen[inorder[i]] = i;
-        }
-        int take = 0;
-        TreeNode* root = constructBST(take, 0, n-1, inorder, preorder, seen);
+        if(preorder.size() == 0) return NULL;
+        int taken = 0;
+        TreeNode* root = constructBST(taken, preorder.size(), preorder, INT_MIN, INT_MAX);
         return root;
     }
 };

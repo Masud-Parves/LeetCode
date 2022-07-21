aeac70256e926699ebbf76837ec8f908a125d032
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-07-21 06:12:50 -0700

    Time: 523 ms (20.80%), Space: 402.6 MB (5.03%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    int helper(int idx, int prev, vector<int>& stones, unordered_map<int, int>& mp, vector<vector<int>>& DP){
+        if(mp[stones[idx]] == stones.size()-1){
+            return 1;
+        }
+
+        int& cache = DP[idx][prev];
+        if(cache != -1){
+            return cache;
+        }
+        
+        cache = 0;
+        bool tmp = false;
+        if(prev-1 != 0 && mp.find(stones[idx]+prev-1) != mp.end()){
+            tmp |= helper(mp[stones[idx]+prev-1], prev-1, stones, mp, DP);
+        }
+        
+        if(mp.find(stones[idx]+prev) != mp.end()){
+            tmp |= helper(mp[stones[idx]+prev], prev, stones, mp, DP);
+        }
+        
+        if(mp.find(stones[idx]+prev+1) != mp.end()){
+            tmp |= helper(mp[stones[idx]+prev+1], prev+1, stones, mp, DP);
+        }
+        return cache = tmp;
+    }
+public:
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+        
+        if(stones[0]+1<stones[1]) return false;
+        
+        unordered_map<int, int>mp;
+        for(int i=0; i<n; i++){
+            mp[stones[i]] = i;
+        }
+        vector<vector<int>> DP(2001, vector<int>(2001, -1));
+        return helper(1, 1, stones, mp, DP);
+    }
+};

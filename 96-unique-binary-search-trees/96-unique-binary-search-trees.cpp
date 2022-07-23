83c0ba7545855316acb9a10728a5f4d5f76b8e3a
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-07-23 06:17:51 -0700

    Time: 3 ms (37.90%), Space: 6.1 MB (37.23%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,19 +1,31 @@
 class Solution {
+private:
+    int helper(int idx, vector<int>& DP){
+        if(idx == 0 ){
+            return 1;
+        }
+        
+        if(idx == 1 || idx == 2){
+            return idx;
+        }
+        
+        int& cache = DP[idx];
+        if(cache != -1){
+            return cache;
+        }
+        
+        int total = 0;
+        for(int i=1; i<=idx; i++){
+            int left = helper(i-1, DP);
+            int right = helper(idx-i, DP);
+            total += left*right;
+        }
+        
+        return cache = total;
+    }
 public:
     int numTrees(int n) {
-        if(n<=1) return 1;
-        
-        vector<int>DP(n+1, 1);
-        
-        for(int nodes=1; nodes<=n; nodes++){
-            int total = 0;
-            for(int root = 1; root<=nodes; root++){
-                int left = root-1;
-                int right = nodes-root;
-                total += DP[left]*DP[right];
-            }
-            DP[nodes] = total;
-        }
-        return DP[n];
+        vector<int>DP(n+1, -1);
+        return helper(n, DP);
     }
 };

e55e3a62dde733443b929b5dcf3be8c9b35e9618
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-04 10:52:21 -0700

    Time: 3 ms (58.76%), Space: 7.9 MB (42.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 private:
     int helper(int idx, int N, vector<int>&nums, vector<int>& DP){
-        if(idx >= N){
-            return 0;
+        if(idx == N){
+            return nums[idx];
         }
+        if(idx<N) return 0;
         
         int& cache = DP[idx];
         if(cache != -1){
             return cache;
         }
         
-        int way1 = helper(idx+1, N, nums, DP);
-        int way2 = nums[idx] + helper(idx+2, N, nums, DP);
+        int way1 = helper(idx-1, N, nums, DP);
+        int way2 = nums[idx] + helper(idx-2, N, nums, DP);
         
         return cache = max(way1, way2);
     }
@@ -19,11 +20,12 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
+        if(n==2) return max(nums[0], nums[1]);
         
         vector<int>DP(n, -1);
-        int way1 = helper(0, n-1, nums, DP);
+        int way1 = helper(n-2, 0, nums, DP);
         fill(DP.begin(), DP.end(), -1);
-        int way2 = helper(1, n, nums, DP);    
+        int way2 = helper(n-1, 1, nums, DP);    
         return max(way1, way2);
     }
 };

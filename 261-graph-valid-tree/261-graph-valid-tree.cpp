a8d53f69e08499ca7a424532eea4de94f5a34413
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-03 19:48:58 -0700

    Time: 34 ms (38.02%), Space: 14 MB (33.93%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,20 +1,4 @@
 class Solution {
-private:
-    bool dfs(int u, int p, vector<vector<int>>& graph, vector<bool>& visited){
-        
-        if(visited[u] == true) return false;
-        
-        visited[u] = true;
-        
-        for(auto& v:graph[u]){
-            if(v != p){
-                if(dfs(v, u, graph, visited) == false){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 public:
     // we can slve the problem using BFS, DFS, DSU, Just check the graph is connected and not contain any circle.
     bool validTree(int n, vector<vector<int>>& edges) {
@@ -26,12 +10,25 @@ public:
             graph[edge[1]].push_back(edge[0]);
         }
         
-        vector<bool> visited(n, false);
-        int ret = dfs(0, -1, graph, visited);
-        if(ret == false) return false;
-        for(int i=0; i<n; i++){
-            if(visited[i] == false) return false;
+        map<int, int>par;
+        par[0] = -1;
+        queue<int>Q;
+        Q.push(0);
+        
+        
+        while(!Q.empty()){
+            int u = Q.front();
+            Q.pop();
+            
+            for(auto& v : graph[u]){
+                if(par[u] == v) continue;
+                if(par.find(v) != par.end()) return false;
+                Q.push(v);
+                par[v] = u;
+            }
+            
         }
-        return true;
+        
+        return par.size() == n;
     }
 };

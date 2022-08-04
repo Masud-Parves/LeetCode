8ab0703f3936f3ef033fc02fe24c371ace323247
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-04 09:53:20 -0700

    Time: 0 ms (100.00%), Space: 7.9 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,31 +1,29 @@
 class Solution {
 private:
-    int helper(int idx, int taken, vector<int>& nums, vector<vector<int>>& DP){
+    int helper(int idx, vector<int>& nums, vector<int>& DP){
         if(idx == 0){
-            if(taken == 0) return nums[idx];
-            return 0;
+            return nums[0];
         }
+        if(idx<0) return 0;
         
-        int& cache = DP[idx][taken];
+        int& cache = DP[idx];
         if(cache != -1){
             return cache;
         }
         
         int take = 0, not_take = 0;
+
+        not_take = helper(idx-1, nums, DP);
+    
+        take = nums[idx] + helper(idx-2, nums, DP);
         
-        
-        
-        not_take = helper(idx-1, 0, nums, DP);
-        if(taken == 0) {
-            take = nums[idx] + helper(idx-1, 1-taken, nums, DP);
-        }
         return cache = max(not_take, take);
     }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        vector<vector<int>>DP(n, vector<int>(2, -1));
-        return helper(n-1, 0, nums, DP);
+        vector<int>DP(n, -1);
+        return helper(n-1, nums, DP);
     }
 };

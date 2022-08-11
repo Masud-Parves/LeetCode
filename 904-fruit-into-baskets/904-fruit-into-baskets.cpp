3141bff505e9ef386e1982896236a8da8597930d
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-11 07:59:10 +0600

    Time: 335 ms (20.29%), Space: 73.3 MB (23.62%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int maxFruits = 0, allowItems = 2;
+        
+        unordered_map<int, int> seen;
+        int l = 0, r = 0, uniqueItems = 0;
+        
+        while(r<n){
+            seen[fruits[r]]++;
+            if(seen[fruits[r]] == 1) uniqueItems++;
+            if(uniqueItems<=allowItems){
+                maxFruits = max(maxFruits, r-l+1);
+            } else {
+                while(uniqueItems>allowItems){
+                    seen[fruits[l]]--;
+                    if(seen[fruits[l]] == 0) uniqueItems--;
+                    l++;
+                }
+                if(uniqueItems <= allowItems){
+                    maxFruits = max(maxFruits, r-l+1);
+                }
+            }
+            r++;
+        }
+        return maxFruits;
+    }
+};

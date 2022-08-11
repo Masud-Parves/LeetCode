da3da4dd1bf19a876a8f9f77df70b1e9e19b9379
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-11 07:38:07 +0600

    Time: 20 ms (64.74%), Space: 8.3 MB (58.26%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        if(n == 0) {
-            return 0;
-        }
+        int window = 0;
         
         unordered_map<char, int> seen;
-        int result = 1;
-        int l = 0;
-        for(int r=0; r<n; r++){
+        int l = 0, r = 0;
+        while(r<n){
             seen[s[r]]++;
-            if(seen[s[r]]>1) {
-                while(seen[s[r]]!=1){
+            if(seen[s[r]] == 1){
+                window = max(window, r-l+1);
+            } else {
+                while(seen[s[r]]>1){
                     seen[s[l]]--;
                     l++;
                 }
+                if(seen[s[r]]<=1) {
+                    window = max(window, r-l+1);
+                }
             }
-            result = max(result , r-l+1);
+            r++;
+            
         }
-        return result;
+        return window;
     }
 };

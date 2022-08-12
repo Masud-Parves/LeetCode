22e3b2113dbc58ea5f26ff4932a76659a65d0242
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-12 09:48:03 +0600

    Time: 18 ms (55.10%), Space: 7 MB (85.28%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.size();    
-        vector<int>cnt(26, 0);
-    
-        int left = 0, right = 0, mostFreq = 0, result = 0;
-        while(right<n){
-            
-            cnt[s[right]-'A']++;
-            
-            mostFreq = max(mostFreq, cnt[s[right]-'A']);
-            right++;
-            if(right-left-mostFreq>k){
-                cnt[s[left]-'A']--;
-                left++;
-            }
-            
-            result = max(result, right-left);
-        }
-            
-        return result;
+        int n = s.size();
         
+        int mostFreq = 0;
+        unordered_map<char, int> seen;
+        
+        int l = 0, result = 0;
+        for(int r=0; r<n; r++){
+            seen[s[r]]++;
+            mostFreq = max(mostFreq, seen[s[r]]);
+            while(r-l+1-mostFreq > k){
+                seen[s[l]]--;
+                l++;
+            }
+            result = max(result, r-l+1);
+        }
+        return result;
     }
 };

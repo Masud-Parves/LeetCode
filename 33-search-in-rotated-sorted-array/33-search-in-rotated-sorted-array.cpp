7e95d847640873eb0aff221c44678f768e7cf28a
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-14 00:54:18 +0600

    Time: 7 ms (53.86%), Space: 11.2 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,26 @@
 class Solution {
-private:
-    int helper(vector<int>& nums, int target, int L, int R){
-        int M = (L+R)/2;
-        
-        if(nums[M] == target) {
-            return M;
-        }
-        
-        if(L>R) return -1;
-        
-        if(nums[L]<=nums[M]){
-            if(nums[L]<=target && target<=nums[M]){
-                return helper(nums, target, L, M-1);
-            } else {
-                return helper(nums, target, M+1, R);
-            }
-        } else {
-            if(nums[M]<=target && target<=nums[R]){
-                return helper(nums, target, M+1, R);
-            }else {
-                return helper(nums, target, L, M-1);
-            }
-        }
-        return -1;
-    }
 public:
     int search(vector<int>& nums, int target) {
         int L = 0, R = nums.size()-1;
         
-        return helper(nums, target, L, R);
+        
+        while(L<=R){
+            int mid = L + (R-L)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[L]<=nums[mid]) {
+                if(nums[L]<=target && target<=nums[mid]){
+                    R = mid-1;
+                } else {
+                    L = mid+1;
+                }
+            } else {
+                if(nums[mid]<= target && target<=nums[R]){
+                    L = mid+1;
+                }else {
+                    R = mid-1;
+                }
+            }
+        }
+        return -1;
     }
 };

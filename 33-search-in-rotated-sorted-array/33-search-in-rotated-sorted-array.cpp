10006d5e0a616f7945f6ca793f7abcc90258f3d2
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-14 02:30:05 +0600

    Time: 7 ms (53.86%), Space: 10.9 MB (95.53%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,44 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int L = 0, R = nums.size()-1;
-        
-        
-        while(L<=R){
-            int mid = L + (R-L)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[L]<=nums[mid]) {
-                if(nums[L]<=target && target<=nums[mid]){
-                    R = mid-1;
-                } else {
-                    L = mid+1;
-                }
+private:
+    int findPivotElement(vector<int>& nums, int target, int start, int end){
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(mid+1<=end && nums[mid]>nums[mid+1]) return mid;
+            else if(mid-1>=start && nums[mid-1]>nums[mid]) return mid-1;
+            else if(nums[start]>nums[mid]) {
+                end = mid-1;
             } else {
-                if(nums[mid]<= target && target<=nums[R]){
-                    L = mid+1;
-                }else {
-                    R = mid-1;
-                }
+                start = mid+1;
             }
         }
         return -1;
+    }
+    
+    int binarySearch(vector<int>& nums, int target, int start, int end){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid]<target){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size()-1;
+        int retIdx = -1;
+        int peakIndex = findPivotElement(nums, target, start, end);
+        //cout << peakIndex << endl;
+        retIdx = binarySearch(nums, target, start, peakIndex);
+        //cout << retIdx << endl;
+        if(retIdx != -1) return retIdx;
+        retIdx = binarySearch(nums, target, peakIndex+1, end);
+        //cout << retIdx << endl;
+        return retIdx;
     }
 };

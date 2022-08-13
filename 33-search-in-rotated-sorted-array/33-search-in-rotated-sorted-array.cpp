0e3d409b0cdcade8075aadd03a631e49bdcf8039
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-14 03:00:18 +0600

    Time: 4 ms (72.82%), Space: 11.1 MB (75.18%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 private:
-    int findPivotElement(vector<int>& nums, int target, int start, int end){
-        while(start < end){
+    int findPivotElement(vector<int>& nums){
+        int start = 0, end = nums.size()-1;
+        while(start <= end){
             int mid = start + (end - start)/2;
             if(mid+1<=end && nums[mid]>nums[mid+1]) return mid;
             else if(mid-1>=start && nums[mid-1]>nums[mid]) return mid-1;
@@ -32,13 +33,13 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0, end = nums.size()-1;
         int retIdx = -1;
-        int peakIndex = findPivotElement(nums, target, start, end);
-        //cout << peakIndex << endl;
-        retIdx = binarySearch(nums, target, start, peakIndex);
-        //cout << retIdx << endl;
-        if(retIdx != -1) return retIdx;
-        retIdx = binarySearch(nums, target, peakIndex+1, end);
-        //cout << retIdx << endl;
-        return retIdx;
+        int pivot = findPivotElement(nums);
+        if(pivot == -1) return binarySearch(nums, target, 0, end);
+        
+        if(nums[pivot] == target) return pivot;
+        
+        if(target>=nums[0]) return binarySearch(nums, target, start, pivot-1);
+        
+        return binarySearch(nums, target, pivot+1, end);
     }
 };

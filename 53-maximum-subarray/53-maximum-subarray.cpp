78e6501c8ff42e9b3350b76fd63f39c81650de7e
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-14 23:40:02 +0600

    Time: 221 ms (16.51%), Space: 67.8 MB (54.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -7,13 +7,11 @@ SC - O(1)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-
         int n = nums.size();
         if(n==1) return nums[0];
 
         int maxSubarraySum = nums[0];
         int maxSumEnd = nums[0];
-        
 
         for(int i=1; i<n; i++){
             maxSumEnd = max(maxSumEnd+nums[i], nums[i]);

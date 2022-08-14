b73acc7d3e33282ed4f67e355d07db966cfdafbe
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-14 23:57:01 +0600

    Time: 2 ms (42.60%), Space: 6.2 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,26 +1,19 @@
+/*
+Problems Link : https://leetcode.com/problems/climbing-stairs/
+TC - O(n)
+SC - O(n) + O(n)Resursion Stack Memory used
+*/
+
 class Solution {
-private:
-    int helper(int idx, vector<int>& DP){
-        if(idx == 0){
-            return 1;
-        }
-
-        int& cache = DP[idx];
-        if(cache != -1){
-            return cache;
-        }
-
-
-        int way1 = 0, way2 = 0;
-        way1 = helper(idx-1, DP);
-        if(idx-2>=0){
-            way2 = helper(idx-2, DP);
-        } 
-        return cache = way1 + way2;
-    }
 public:
     int climbStairs(int n) {
-        vector<int> DP(n+1, -1);
-        return helper(n, DP);
+        vector<int> DP(n+1, 0);
+        DP[0] = 1;
+
+        for(int i=1; i<=n; i++){
+            DP[i] = DP[i-1];
+            DP[i]+= (i-2>=0) ? DP[i-2] : 0;
+        }
+        return DP[n];
     }
 };

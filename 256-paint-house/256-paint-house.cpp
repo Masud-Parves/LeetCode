d3b35ebf1ddbead1b67bf899b7642f28d756f9ca
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-15 15:43:05 +0600

    Time: 9 ms (80.41%), Space: 9.7 MB (58.09%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
 int minCost(vector<vector<int>>& costs) {
         int n = costs.size();
-        vector<vector<int>> DP(n, vector<int>(4));
+        vector<int> previous(4), current(4);
 
-        DP[0][0] = min(costs[0][1], costs[0][2]);
-        DP[0][1] = min(costs[0][0], costs[0][2]);
-        DP[0][2] = min(costs[0][0], costs[0][1]);
-        DP[0][3] = min(costs[0][0], min(costs[0][1], costs[0][2]));
+        previous[0] = min(costs[0][1], costs[0][2]);
+        previous[1] = min(costs[0][0], costs[0][2]);
+        previous[2] = min(costs[0][0], costs[0][1]);
+        previous[3] = min(costs[0][0], min(costs[0][1], costs[0][2]));
 
         for(int i=1; i<n; i++){
             for(int last=0; last<4; last++){
-                DP[i][last] = INT_MAX;
+                current[last] = INT_MAX;
                 for(int k=0; k<3; k++){
                     if(last == k) continue;
-                    DP[i][last] = min(DP[i][last], costs[i][k] + DP[i-1][k]);
+                    current[last] = min(current[last], costs[i][k] + previous[k]);
                 }
             }
+            previous = current;
         }
 
-        return DP[n-1][3];
+        return previous[3];
     }
 };

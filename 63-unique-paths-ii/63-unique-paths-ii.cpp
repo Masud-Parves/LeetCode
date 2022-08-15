f0c47002be525132150c8423383216b4ee50fc38
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-15 19:18:29 +0600

    Time: 4 ms (66.93%), Space: 7.9 MB (12.72%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
-private:
-    int helper(int r, int c, int row, int col, vector<vector<int>>& grid, vector<vector<int>>& DP){
-        if(r<0 || r == row || c<0 || c == col || grid[r][c] == 1) return 0;
-        if(r == row-1 && c == col-1){
-            return 1;
-        }
-      
-        int& cache = DP[r][c];
-        if(cache != -1){
-            return cache;
-        }
-
-        int way1, way2;
-        way1 = helper(r, c+1, row, col, grid, DP);
-        way2 = helper(r+1, c, row, col, grid, DP);
-        return cache = (way1 + way2);
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int row = obstacleGrid.size(), col = obstacleGrid[0].size();
-        vector<vector<int>> DP(row, vector<int>(col, -1));
-        return helper(0, 0, row, col, obstacleGrid, DP);
+        if(obstacleGrid[0][0] == 1) return 0;
+        vector<vector<int>> DP(row, vector<int>(col, 0));
+        
+        for(int r = 0; r<row ; r++){
+          for(int c = 0; c<col ; c++){
+              if(r==0 && c==0) {
+                  DP[r][c] = 1;
+              } else if(obstacleGrid[r][c] == 1){
+                  DP[r][c] = 0;
+              } else {
+                  if(r>0) DP[r][c] += DP[r-1][c];
+                  if(c>0) DP[r][c] += DP[r][c-1];
+              }
+          }
+        }
+        return DP[row-1][col-1];
     }
 };

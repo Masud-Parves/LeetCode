8798cb607b5837eb74f0e436a18af3df6e6dd4b2
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-15 12:32:54 +0600

    Time: 188 ms (70.21%), Space: 213.3 MB (23.66%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,41 +1,43 @@
 class Solution {
 private:
-    int helper(int idx, int prev, vector<int>& stones, unordered_map<int, int>& seen, vector<vector<int>>& DP, int n){
+    int helper(int idx, int prevIdx, unordered_map<int, int>& seen, vector<int>& stones, vector<vector<int>>& DP, int n){
         if(seen[stones[idx]] == n-1){
             return 1;
         }
 
-        int& cache = DP[idx][prev];
+
+        int& cache = DP[idx][prevIdx];
         if(cache != -1){
             return cache;
         }
-        
+
         cache = 0;
         bool state = false;
-        if(prev-1 != 0 && seen.find(stones[idx]+prev-1) != seen.end()){
-            state |= helper(seen[stones[idx]+prev-1], prev-1, stones, seen, DP, n);
+        if(prevIdx-1!=0 && seen.find(stones[idx]+prevIdx-1) != seen.end()){
+            state |= helper(seen[stones[idx]+prevIdx-1], prevIdx-1, seen, stones, DP, n);
         }
-        
-        if(seen.find(stones[idx]+prev) != seen.end()){
-            state |= helper(seen[stones[idx]+prev], prev, stones, seen, DP, n);
+
+        if(seen.find(stones[idx]+prevIdx) != seen.end()){
+            state |= helper(seen[stones[idx]+prevIdx], prevIdx, seen, stones, DP, n);
         }
-        
-        if(seen.find(stones[idx]+prev+1) != seen.end()){
-            state |= helper(seen[stones[idx]+prev+1], prev+1, stones, seen, DP, n);
+
+        if(seen.find(stones[idx]+prevIdx+1) != seen.end()){
+            state |= helper(seen[stones[idx]+prevIdx+1], prevIdx+1, seen, stones, DP, n);
         }
         return cache = state;
     }
 public:
     bool canCross(vector<int>& stones) {
         int n = stones.size();
-        
-        if(stones[0]+1<stones[1]) return false;
-        
-        unordered_map<int, int>seen;
+
+        if(stones[0] + 1<stones[1]) return false;
+        unordered_map<int, int> seen;
         for(int i=0; i<n; i++){
             seen[stones[i]] = i;
         }
+
         vector<vector<int>> DP(n+1, vector<int>(n+1, -1));
-        return helper(1, 1, stones, seen, DP, n);
+        return helper(1, 1, seen, stones, DP, n);
     }
+    
 };

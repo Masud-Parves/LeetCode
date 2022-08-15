c08ed6eaeba678e36106ccef832bff18dbae06ea
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-15 15:38:40 +0600

    Time: 12 ms (62.88%), Space: 10.3 MB (17.82%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -1,31 +1,24 @@
 class Solution {
-    int helper(int idx, int prev, vector<vector<int>>& costs, vector<vector<int>>& DP, int n){
-        if(idx == n-1){
-            int minVal = INT_MAX;
-            for(int i=0; i<3; i++){
-                if(prev == i) continue;
-                minVal = min(minVal, costs[idx][i]);
-            }
-            return minVal;
-        }
-        
-        int& cache = DP[idx][prev];
-        if(cache != -1){
-            return cache;
-        }
-        
-        cache = INT_MAX/2;
-        int ch1, ch2;
-        for(int i=0; i<3; i++){
-            if(i == prev) continue;
-            cache = min(cache, costs[idx][i] + helper(idx+1, i, costs, DP, n));
-        }
-        return cache;
-    }
 public:
-    int minCost(vector<vector<int>>& costs) {
+int minCost(vector<vector<int>>& costs) {
         int n = costs.size();
-        vector<vector<int>> DP(n, vector<int>(4, -1));
-        return helper(0, 3, costs, DP, n);
+        vector<vector<int>> DP(n, vector<int>(4, INT_MAX/2));
+
+        DP[0][0] = min(costs[0][1], costs[0][2]);
+        DP[0][1] = min(costs[0][0], costs[0][2]);
+        DP[0][2] = min(costs[0][0], costs[0][1]);
+        DP[0][3] = min(costs[0][0], min(costs[0][1], costs[0][2]));
+
+        for(int i=1; i<n; i++){
+            for(int last=0; last<4; last++){
+                DP[i][last] = INT_MAX;
+                for(int k=0; k<3; k++){
+                    if(last == k) continue;
+                    DP[i][last] = min(DP[i][last], costs[i][k] + DP[i-1][k]);
+                }
+            }
+        }
+
+        return DP[n-1][3];
     }
 };

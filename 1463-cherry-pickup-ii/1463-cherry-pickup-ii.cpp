a42a626fa8c6c48204f745e837b6c3f36695935d
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-16 23:25:31 +0600

    Time: 387 ms (11.37%), Space: 17.5 MB (8.46%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,31 +1,42 @@
+/*
+    Problems Link : https://leetcode.com/problems/cherry-pickup-ii/
+    TC - O(rows*cols*cols)
+    SC - O(rows*cols*cols)
+*/
+
 class Solution {
-private:
-    int helper(int r, int c1, int c2, vector<vector<int>>& grid, vector<vector<vector<int>>>& DP, int rows, int cols){
-        if(r<0 || c1<0 || c2<0 || c1==cols || c2==cols) return INT_MIN;
-        if(r == rows-1){
-            if(c1==c2) return grid[r][c1];
-            return grid[r][c1] + grid[r][c2];
-        }
-        
-        int& cache = DP[r][c1][c2];
-        if(cache != -1){
-            return cache;
-        }
-        
-        cache = INT_MIN;
-        for(int c1i=-1; c1i<=1; c1i++){
-            for(int c2i =-1; c2i<=1; c2i++){
-                int c = c1+c1i, cc = c2+c2i, cherry = grid[r][c1];
-                if(c1 != c2) cherry +=  grid[r][c2];
-                cache = max(cache, cherry + helper(r+1, c, cc, grid, DP, rows, cols));
-            }
-        }
-        return cache;
-    }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
-        vector<vector<vector<int>>> DP(rows+1, vector<vector<int>>(cols+1, vector<int>(cols+1, -1)));
-        return helper(0, 0, cols-1, grid, DP, rows, cols);
+        vector<vector<vector<int>>> DP(rows+1, vector<vector<int>>(cols+1, vector<int>(cols+1, INT_MIN)));
+
+        for(int c1=0; c1<cols; c1++){
+            for(int c2=0; c2<cols ; c2++){
+                DP[rows-1][c1][c2] = grid[rows-1][c1];
+                if(c1 != c2) DP[rows-1][c1][c2] += grid[rows-1][c2];
+            }
+        }
+
+        for(int r=rows-2; r>=0; r--){
+            for(int c1=0; c1<cols; c1++){
+                for(int c2=0; c2<cols; c2++){
+                    int result = INT_MIN;
+
+                    for(int c1i=-1; c1i<=1; c1i++){
+                        for(int c2i=-1; c2i<=1; c2i++){
+                            int cherry = grid[r][c1];
+                            if(c1!=c2) cherry+=grid[r][c2];
+                            int nextC1 = c1 + c1i, nextC2 = c2 + c2i;
+                            if(nextC1<0 || nextC2<0 || nextC1 == cols || nextC2 == cols) continue;
+                            result = max(result, cherry+DP[r+1][nextC1][nextC2]);
+                        }
+                    }
+                    DP[r][c1][c2] = result;
+
+                }
+            }
+        }
+
+        return DP[0][0][cols-1];
     }
 };

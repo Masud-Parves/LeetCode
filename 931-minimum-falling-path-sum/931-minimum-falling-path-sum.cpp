1e5a1eeb48302e2424f2d132e237aa2a092cebb1
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-16 07:50:52 +0600

    Time: 30 ms (24.59%), Space: 10.3 MB (53.52%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -17,13 +17,32 @@ private:
         return cache = matrix[r][c] + min(way1, min(way2, way3));
     }
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+int minFallingPathSum(vector<vector<int>>& matrix) {
         int rows = matrix.size(), cols = matrix[0].size();
-        vector<vector<int>> DP(rows, vector<int>(cols, -1));
+        vector<vector<int>> DP(rows, vector<int>(cols, INT_MAX/2));
         int result = INT_MAX;
-        for(int c=0; c<cols; c++){
-            result = min(result, helper(0, c, rows, cols, matrix, DP));
+        
+
+        for(int i=0; i<cols; i++){
+            DP[0][i] = matrix[0][i];
+        }
+
+        for(int r=1; r<rows; r++){
+            for(int c=0; c<cols ; c++){
+                DP[r][c] = min(DP[r][c], matrix[r][c] + DP[r-1][c]);
+                if(c+1<cols) {
+                    DP[r][c] = min(DP[r][c], matrix[r][c] + DP[r-1][c+1]);
+                }
+                if(c>0) {
+                    DP[r][c] = min(DP[r][c], matrix[r][c] + DP[r-1][c-1]);
+                }
+            }
+        }
+
+        for(int i=0; i<cols; i++){
+            result = min(result, DP[rows-1][i]);
         }
         return result;
     }
+
 };

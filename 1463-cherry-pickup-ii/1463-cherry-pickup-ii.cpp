4e1d9fd1f271a64a775957929c1592415db0fd3d
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-16 23:31:51 +0600

    Time: 316 ms (16.81%), Space: 16.9 MB (51.89%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,23 +1,25 @@
 /*
     Problems Link : https://leetcode.com/problems/cherry-pickup-ii/
     TC - O(rows*cols*cols)
-    SC - O(rows*cols*cols)
+    SC - O(cols*cols)
 */
 
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
-        vector<vector<vector<int>>> DP(rows+1, vector<vector<int>>(cols+1, vector<int>(cols+1, INT_MIN)));
+        vector<vector<int>> previousRow(cols, vector<int>(cols, 0));
+
 
         for(int c1=0; c1<cols; c1++){
             for(int c2=0; c2<cols ; c2++){
-                DP[rows-1][c1][c2] = grid[rows-1][c1];
-                if(c1 != c2) DP[rows-1][c1][c2] += grid[rows-1][c2];
+                previousRow[c1][c2] = grid[rows-1][c1];
+                if(c1 != c2) previousRow[c1][c2] += grid[rows-1][c2];
             }
         }
 
         for(int r=rows-2; r>=0; r--){
+            vector<vector<int>> currentRow(cols, vector<int>(cols, 0));
             for(int c1=0; c1<cols; c1++){
                 for(int c2=0; c2<cols; c2++){
                     int result = INT_MIN;
@@ -28,15 +30,15 @@ public:
                             if(c1!=c2) cherry+=grid[r][c2];
                             int nextC1 = c1 + c1i, nextC2 = c2 + c2i;
                             if(nextC1<0 || nextC2<0 || nextC1 == cols || nextC2 == cols) continue;
-                            result = max(result, cherry+DP[r+1][nextC1][nextC2]);
+                            result = max(result, cherry+previousRow[nextC1][nextC2]);
                         }
                     }
-                    DP[r][c1][c2] = result;
-
+                    currentRow[c1][c2] = result;
                 }
             }
+            previousRow = currentRow;
         }
 
-        return DP[0][0][cols-1];
+        return previousRow[0][cols-1];
     }
 };

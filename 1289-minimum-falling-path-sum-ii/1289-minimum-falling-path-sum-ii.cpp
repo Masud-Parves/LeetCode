d6a27abdd5507d7a1276fb42cc67a5213557356d
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-16 08:35:08 +0600

    Time: 1476 ms (14.72%), Space: 15.9 MB (36.75%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
-private:
-    int helper(int r, int prev, vector<vector<int>>& grid, int rows, int cols, vector<vector<int>>& DP){
-        if(r == rows){
-            return 0;
-        }
-        int& cache = DP[r][prev];
-        if(cache != -1){
-            return cache;
-        }
-        
-        cache = INT_MAX;
-        for(int c=0; c<cols; c++){
-            if(c==prev) continue;
-            cache = min(cache , grid[r][c] + helper(r+1, c, grid, rows, cols, DP));
-        }
-        return cache;
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int rows = grid.size(), cols = grid[0].size();
-        vector<vector<int>> DP(rows+1, vector<int>(cols+1, -1));
-        return helper(0, cols, grid, rows, cols, DP);
+        int n = grid.size();
+        vector<vector<int>> DP(n+1, vector<int>(n+1, INT_MAX/2));
+        int result = INT_MAX;
+        for(int i=0; i<n; i++){
+            DP[0][i] = grid[0][i];
+        }
+
+        for(int r=1; r<n ; r++){
+            for(int p=0; p<n; p++){
+                for(int c=0; c<n; c++){
+                    if(p==c) continue;
+                    DP[r][c] = min(DP[r][c], grid[r][c] + DP[r-1][p]);
+                }
+            }
+        }
+
+        for(int c=0; c<n; c++){
+            result = min(result, DP[n-1][c]);
+        }
+        return result;
     }
 };

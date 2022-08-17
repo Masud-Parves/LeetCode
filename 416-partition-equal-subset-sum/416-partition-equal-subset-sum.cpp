bd799376258767be5d41a56587150b157d2cec9c
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-17 09:04:37 +0600

    Time: 451 ms (33.01%), Space: 9.7 MB (86.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,9 +1,3 @@
-/*
-    Problems Link : https://leetcode.com/problems/partition-equal-subset-sum/
-    TC - O(rows*cols*cols)
-    SC - O(rows*cols*cols) + O(rows) Recursion Stack Memory.
-*/
-
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
@@ -14,21 +8,19 @@ public:
         }
         if(sum&1 || n<=1) return false;
         int target = sum/2;
-        vector<vector<bool>> DP(n, vector<bool>(target+1, false));
+        vector<bool> nextRow(target+1, false), currentRow(target+1, false);
 
-
-        for(int i=0; i<n; i++){
-            DP[i][0] = true;
-        }
-        DP[n-1][nums[n-1]] = true;
+        nextRow[0] = true;
+        nextRow[nums[n-1]] = true;
 
         for(int i=n-2; i>=0; i--){
             for(int t=1; t<=target; t++){
-                DP[i][t] = DP[i+1][t];
-                if(t-nums[i]>=0) DP[i][t] = DP[i][t] | DP[i+1][t-nums[i]];
+                currentRow[t] = nextRow[t];
+                if(t-nums[i]>=0) currentRow[t] = currentRow[t] | nextRow[t-nums[i]];
             }
+            nextRow = currentRow;
         }
 
-        return DP[0][target];
+        return nextRow[target];
     }
 };

ccb265907e9aed9a4fda3d4fb7909e57ad4ab09b
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-17 12:24:40 +0600

    Time: 14 ms (81.50%), Space: 9.5 MB (50.17%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,9 +1,3 @@
-/*
-    Problems Link : https://leetcode.com/problems/target-sum/
-    TC - O(n*target)
-    SC - O(n*target)
-*/
-
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -16,20 +10,20 @@ public:
         int amount = totalSum - target;
         if(amount< 0 || amount%2 != 0) return 0;
         amount = (totalSum-target)/2;
-        vector<vector<int>> DP(n, vector<int>(amount+1, 0));
+        vector<int>previousRow(amount+1, 0), currentRow(amount+1, 0);
         
-        DP[0][0] = 1 + (nums[0] == 0);
-        if(nums[0] != 0 && nums[0]<=amount) DP[0][nums[0]] = 1;      
+        previousRow[0] = 1 + (nums[0] == 0);
+        if(nums[0] != 0 && nums[0]<=amount) previousRow[nums[0]] = 1;      
         
-
         for(int idx = 1; idx<n ; idx++){
             for(int t = 0; t<=amount ; t++){
-                DP[idx][t] =  DP[idx-1][t];
+                currentRow[t] =  previousRow[t];
                 if(nums[idx]<=t) {
-                    DP[idx][t] += DP[idx-1][t-nums[idx]];
+                    currentRow[t] += previousRow[t-nums[idx]];
                 }
             }
+            previousRow = currentRow;
         }
-        return DP[n-1][amount];
+        return previousRow[amount];
     }
 };

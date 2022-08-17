0a0498ade34a63c372195cce8dfb60d040fe742c
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-17 22:26:15 +0600

    Time: 167 ms (39.64%), Space: 18.6 MB (35.68%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> DP(n, vector<int>(amount+1, INT_MAX/2));
+        vector<int>previousRow(amount+1, INT_MAX/2), currentRow(amount+1, INT_MAX/2);
         
         for(int cash=0; cash<=amount; cash++){
-            if(cash%coins[0] == 0) DP[0][cash] = cash/coins[0];
-            else DP[0][cash] = INT_MAX/2;
+            if(cash%coins[0] == 0) previousRow[cash] = cash/coins[0];
+            else previousRow[cash] = INT_MAX/2;
         }
 
         for(int idx=1; idx<n; idx++){
             for(int cash=0; cash<=amount; cash++){
-                int non_pick = DP[idx-1][cash], pick = INT_MAX/2; 
+                int non_pick = previousRow[cash], pick = INT_MAX/2; 
                 if(cash>=coins[idx]){
-                    pick = 1 + DP[idx][cash-coins[idx]];
+                    pick = 1 + currentRow[cash-coins[idx]];
                 }
-                DP[idx][cash] = min(pick, non_pick);
+                currentRow[cash] = min(pick, non_pick);
             }
+            previousRow = currentRow;
         }
-        return (DP[n-1][amount] == INT_MAX/2) ? -1 : DP[n-1][amount];
+        return (previousRow[amount] == INT_MAX/2) ? -1 : previousRow[amount];
     }
 };

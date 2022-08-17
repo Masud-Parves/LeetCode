477744f875dfef7ecdca3719b5356cd5575f25a6
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-17 22:47:46 +0600

    Time: 79 ms (22.01%), Space: 18.4 MB (49.62%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -21,7 +21,22 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         if(amount == 0) return 1;
-        vector<vector<int>>DP(n, vector<int>(amount+1, -1));
-        return calculateWays(n-1, amount, coins, DP);
+        vector<vector<int>>DP(n, vector<int>(amount+1, 0));
+
+        for(int c=0; c<=amount; c++){
+        	DP[0][c] = (c%coins[0] == 0);
+        }
+
+        for(int idx=1; idx<n; idx++){
+        	for(int c=0; c<=amount ; c++){
+        		int way1 = DP[idx-1][c], way2=0;
+        		if(coins[idx]<=c){
+        			way2 = DP[idx][c-coins[idx]];
+        		}
+                DP[idx][c] = (way1+way2);
+        	}
+        }
+
+        return DP[n-1][amount];
     }
 };

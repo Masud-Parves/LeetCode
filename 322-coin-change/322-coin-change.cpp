7e10e2eee1048f458250a1a9214ebde8b20b7c28
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-17 21:47:24 +0600

    Time: 139 ms (55.76%), Space: 45.6 MB (24.52%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,31 +1,28 @@
 class Solution {
-public:
-    
-    
-    int solve(int idx, int amount, vector<int>& coins, vector<vector<int>>& DP){
-        
-        if(amount == 0){
-            return 0;
+private:
+    int calculateMinimumCoins(int idx, int amount, vector<int>& coins, int n, vector<vector<int>>& DP){
+        if(idx == n-1){
+            if(amount%coins[idx]==0) return amount/coins[idx];
+            return INT_MAX/2;
         }
-        if(idx < 0) return INT_MAX/2;
-        
-        
-        int &cache = DP[idx][amount];
-        if(cache != -1) return cache;
-        
-        
-        cache = solve(idx-1, amount, coins, DP);
-        
+
+        int& cache = DP[idx][amount];
+        if(cache != -1){
+            return cache;
+        }
+
+        int way1 = 0, way2 = INT_MAX/2;
+        way1 = calculateMinimumCoins(idx+1, amount, coins, n, DP);
         if(coins[idx]<=amount){
-            cache = min(cache, 1+solve(idx, amount-coins[idx], coins, DP));
+            way2 = 1 + calculateMinimumCoins(idx, amount-coins[idx], coins, n, DP);
         }
-        return cache;
+        return cache = min(way1, way2);
     }
-    
+public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> DP(n, vector<int>(amount+1, -1));
-        int ret = solve(n-1, amount, coins, DP);
-        return ret>=INT_MAX/2 ? -1 : ret;
+        int result = calculateMinimumCoins(0, amount, coins, n, DP);
+        return (result == INT_MAX/2) ? -1 : result;
     }
 };

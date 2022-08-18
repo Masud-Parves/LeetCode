2c1aaaa84726661940f87d1e100396e636d8b7f4
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-19 00:21:10 +0600

    Time: 1644 ms (8.37%), Space: 292.2 MB (8.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
-    
-    int calculateLIS(int now, int pre, vector<int>& nums, vector<vector<int>>& DP){
-        if(now == nums.size()) return 0;
-        
-        int &cache = DP[now][pre+1];
-        if(cache != -1){
-            return cache;
-        }
-        
-        cache = calculateLIS(now+1, pre, nums, DP);
-        if(pre == -1 || nums[now] > nums[pre]){
-            cache = max(cache, 1+calculateLIS(now+1, now, nums, DP));
-        }
-        return cache;
-    }
-    
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        // recursion code 
-        //vector<vector<int>> DP(n+1, vector<int>(n+1, -1));
-        //int result = calculateLIS(0, -1, nums, DP);
-        
-        
-        // Binary Search for Efficient LIS
-        vector<int>result;
-        result.push_back(nums[0]);
-        for(int i=1; i<n; i++){
-            if(nums[i] > result.back()){
-                result.push_back(nums[i]);
-            } else {
-                int low_idx = lower_bound(result.begin(), result.end(), nums[i]) - result.begin();
-                result[low_idx] = nums[i];
-            }
+        vector<vector<int>> DP(n+1, vector<int>(n+1, 0));
+
+        for(int i=n-1; i>=0; i--){
+        	for(int p=i-1; p>=-1; p--){
+        		DP[i][p+1] = DP[i+1][p+1];
+        		if(p == -1 || nums[p]<nums[i]){
+        			DP[i][p+1] = max(DP[i][p+1], 1 + DP[i+1][i+1]); 
+        		}
+        	}
         }
-        
-        return result.size();
+
+        return DP[0][0];
     }
 };

072c7ed13f2234403c62d60dc0f9cedd523ae103
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-18 22:05:54 +0600

    Time: 65 ms (72.80%), Space: 27.6 MB (27.33%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,43 +1,34 @@
 class Solution {
-public:
-    
-    bool isWildcardMatching(int idx1, string& s, int idx2, string& p, vector<vector<int>>& DP){
-        
-        
+private:
+    int helper(int idx1, int idx2, string& s, string& p, vector<vector<int>>& DP){
         if(idx1<0 && idx2<0) return true;
-        
         if(idx2<0) return false;
-        
         if(idx1<0){
-            for(int i=0; i<=idx2 ; i++){
-                if(p[i] != '*'){
-                    return false;
-                }
+            for(int i=0; i<=idx2; i++){
+                if(p[i] != '*') return false;
             }
             return true;
         }
         
-        int &cache = DP[idx1][idx2];
-        if(cache != -1) return cache;
+        int& cache = DP[idx1][idx2];
+        if(cache != -1){
+            return cache;
+        }
+        
         
         if(s[idx1] == p[idx2] || p[idx2] == '?'){
-            return cache = isWildcardMatching(idx1-1, s, idx2-1, p, DP);
+            return cache = helper(idx1-1, idx2-1, s, p, DP);
         }
         
-        if(p[idx2] == '*'){
-            cache = isWildcardMatching(idx1, s, idx2-1, p, DP) | isWildcardMatching(idx1-1, s, idx2, p, DP);
-        } else {
-            cache = false;
-        }
-        
+        if(p[idx2] == '*') {
+            cache = helper(idx1, idx2-1, s, p, DP) | helper(idx1-1, idx2, s, p, DP);
+        } else cache = false;
         return cache;
     }
-    
+public:
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        
+        int n = s.size(), m = p.size();
         vector<vector<int>> DP(n, vector<int>(m, -1));
-        return isWildcardMatching(n-1, s, m-1, p, DP);
+        return helper(n-1, m-1, s, p, DP);
     }
 };

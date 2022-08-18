961f6a8329ee3666fa0bb7d80b13b078c289b6ac
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-18 22:31:22 +0600

    Time: 169 ms (19.64%), Space: 11.2 MB (70.10%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,45 +1,14 @@
 class Solution {
-private:
-    int helper(int idx1, int idx2, string& s, string& p, vector<vector<int>>& DP){
-        if(idx1<0 && idx2<0) return true;
-        if(idx2<0) return false;
-        if(idx1<0){
-            for(int i=0; i<=idx2; i++){
-                if(p[i] != '*') return false;
-            }
-            return true;
-        }
-        
-        int& cache = DP[idx1][idx2];
-        if(cache != -1){
-            return cache;
-        }
-        
-        cache = false;
-        if(s[idx1] == p[idx2] || p[idx2] == '?'){
-            return cache = helper(idx1-1, idx2-1, s, p, DP);
-        }
-        
-        if(p[idx2] == '*') {
-            cache = helper(idx1, idx2-1, s, p, DP) | helper(idx1-1, idx2, s, p, DP);
-        }
-        return cache;
-    }
 public:
 bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
         vector<vector<bool>> DP(n+1, vector<bool>(m+1, false));
-		for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
-            DP[0][j + 1] = true;
-        }
-
         DP[0][0] = true;
         for(int i=0; i< m; i++){
             if(p[i] == '*') {
             	DP[0][i+1] = true;
             } else break;
         }
-
 
         for(int i=1; i<=n ; i++){
         	for(int j=1; j<=m; j++){
@@ -50,7 +19,6 @@ bool isMatch(string s, string p) {
         		}
         	}
         }
-
 
         return DP[n][m];
     }

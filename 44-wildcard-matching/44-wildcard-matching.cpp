969ab1638342f16f91b1303db7ba11fb1f9cb5d4
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-18 22:25:48 +0600

    Time: 154 ms (23.56%), Space: 11.4 MB (58.41%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -26,9 +26,32 @@ private:
         return cache;
     }
 public:
-    bool isMatch(string s, string p) {
+bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<vector<int>> DP(n, vector<int>(m, -1));
-        return helper(n-1, m-1, s, p, DP);
+        vector<vector<bool>> DP(n+1, vector<bool>(m+1, false));
+		for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
+            DP[0][j + 1] = true;
+        }
+
+        DP[0][0] = true;
+        for(int i=0; i< m; i++){
+            if(p[i] == '*') {
+            	DP[0][i+1] = true;
+            } else break;
+        }
+
+
+        for(int i=1; i<=n ; i++){
+        	for(int j=1; j<=m; j++){
+        		if(s[i-1] == p[j-1] || p[j-1] == '?') {
+        			DP[i][j] = DP[i-1][j-1];
+        		}else if(p[j-1] == '*') {
+        			DP[i][j] = DP[i-1][j] | DP[i][j-1];
+        		}
+        	}
+        }
+
+
+        return DP[n][m];
     }
 };

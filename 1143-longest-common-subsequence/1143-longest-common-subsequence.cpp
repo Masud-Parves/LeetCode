fc1593baa64865ffe71ae170e4ee58ec7623074b
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-18 12:21:17 +0600

    Time: 33 ms (57.36%), Space: 13.3 MB (11.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,25 +1,25 @@
 class Solution {
+private:
+	int getLCS(int idx1, int idx2, string& text1, string& text2, vector<vector<int>>& DP){
+		if(idx1==text1.size() || idx2==text2.size()){
+			return 0;
+		}
+
+		int& cache = DP[idx1][idx2];
+		if(cache != -1){
+			return cache;
+		}
+
+		if(text1[idx1] == text2[idx2]){
+			return cache = 1+getLCS(idx1+1, idx2+1, text1, text2, DP);
+		}
+		return cache = max(getLCS(idx1+1, idx2, text1, text2, DP), getLCS(idx1, idx2+1, text1, text2, DP));
+
+	}
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<int>previousRow(m, 0), currentRow(m, 0);
-
-        for(int idx1=0; idx1<n; idx1++){
-        	for(int idx2 = 0; idx2<m; idx2++){
-        		int way1 = 0, way2 = 0;
-        		if(text1[idx1] == text2[idx2]){
-        			way1 = 1;
-        			if(idx1-1>=0 && idx2-1>=0){
-        				way1 += previousRow[idx2-1];
-        			} 
-        		} else {
-        			if(idx1-1>=0) way2 = previousRow[idx2];
-        			if(idx2-1>=0) way2 = max(way2, currentRow[idx2-1]);
-        		}
-                currentRow[idx2] = max(way1, way2);
-        	}
-        	previousRow = currentRow;
-        }
-        return previousRow[m-1];
+        vector<vector<int>> DP(n, vector<int>(m, -1));
+        return getLCS(0, 0, text1, text2, DP);
     }
 };

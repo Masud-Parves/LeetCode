f774c9f308d6794ac71db76d1e09e976253bd532
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-18 16:43:43 +0600

    Time: 78 ms (54.29%), Space: 27.2 MB (7.16%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,29 +1,27 @@
 class Solution {
-public:
-    int LPS(string& s1, string& s2){
-        int n = s1.size();
-        vector<int> previousRow(n+1, 0);
-        vector<int> currentRow(n+1, 0);
+private:
+	int calculateLCS(int id1, int id2, string& s1, string& s2, vector<vector<int>>& DP){
+        if(id1< 0 || id2<0) return 0;
+
+        int &ret = DP[id1][id2];
+        if(ret != -1) return ret;
         
-        for(int i = 1; i<=n ; i++){
-            for(int j = 1; j<=n ; j++){
-                if(s1[i-1] == s2[j-1]){
-                    currentRow[j] = 1 + previousRow[j-1];
-                } else {
-                    currentRow[j] = max(previousRow[j], currentRow[j-1]);
-                }
-            }
-            previousRow = currentRow;
+        if(s1[id1] == s2[id2]){
+            return 1 + calculateLCS(id1-1, id2-1, s1, s2, DP);
         }
         
-        return previousRow[n];
-        
+        return ret = max(calculateLCS(id1-1, id2, s1, s2, DP), calculateLCS(id1, id2-1, s1, s2, DP));
     }
     
-    
+    int getLCS(string& s1, string& s2){
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> DP (n, vector<int>(m, -1));
+        return calculateLCS(n-1, m-1, s1, s2, DP);
+    }
+public:
     int minInsertions(string s) {
-        string t = s;
-        reverse(t.begin(), t.end());
-        return s.size() - LPS(s, t);
+        string s2 = s;
+        reverse(s.begin(), s.end());
+        return s.size() - getLCS(s, s2);
     }
 };

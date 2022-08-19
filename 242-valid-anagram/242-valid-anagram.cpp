81f010f6730bd27b78681dc834a009f34307db25
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-20 01:05:30 +0600

    Time: 12 ms (64.75%), Space: 7.4 MB (46.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,16 +1,21 @@
+// Approach 01 : hash
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        
-        vector<int>cnt(26, 0);
-        for(int i=0; i<s.length(); i++){
-            cnt[s[i]-'a']++;
-            cnt[t[i]-'a']--;
+    		// Time : O(n)
+        // Spce : O(n)
+        // Hint : Two Pass 
+    		if(s.size()!=t.size()) return false;
+        unordered_map<char, int> cntS, cntT;
+        for(int i=0; i<s.size(); i++){
+        		cntS[s[i]]++;
+            cntT[t[i]]++;
         }
         
-        for(int i=0; i<26; i++){
-            if(cnt[i] != 0) return false;
+        
+        for(auto cnt : cntS){
+        	if(cnt.second != cntT[cnt.first]) return false;
         }
         return true;
     }

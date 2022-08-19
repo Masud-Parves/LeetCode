a239a9ccbb25a2a487a0192f797240ca9e2b909f
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-20 01:08:05 +0600

    Time: 24 ms (19.76%), Space: 7.4 MB (7.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,21 +1,20 @@
-// Approach 01 : hash
+// Approach 02: frequiency count
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    		// Time : O(n)
+    	// Time : O(n)
         // Spce : O(n)
-        // Hint : Two Pass 
-    		if(s.size()!=t.size()) return false;
-        unordered_map<char, int> cntS, cntT;
+        // Hint : One O(n) space 
+    	if(s.size()!=t.size()) return false;
+        unordered_map<char, int> seen;
         for(int i=0; i<s.size(); i++){
-        		cntS[s[i]]++;
-            cntT[t[i]]++;
+        		seen[s[i]]++;
+        		seen[t[i]]--;
         }
         
-        
-        for(auto cnt : cntS){
-        	if(cnt.second != cntT[cnt.first]) return false;
+        for(auto m : seen){
+        	if(m.second != 0) return false;
         }
         return true;
     }

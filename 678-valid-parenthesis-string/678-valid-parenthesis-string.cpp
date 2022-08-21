6ce951645bc67e534e60907dcb532232394000c3
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-21 19:06:33 +0600

    Time: 0 ms (100.00%), Space: 6 MB (83.30%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,32 +1,31 @@
 class Solution {
-private:
-    bool helper(int idx, string& s, int take, vector<vector<int>>& DP){
-        if(take <0 || idx > s.size()){
-            return 0;
-        }
-        if(idx == s.size() && take == 0){
-            return 1;
-        }
-        
-        int& cache = DP[idx][take];
-        if(cache != -1){
-            return cache;
-        }
-        
-        if(s[idx] == '*'){
-           cache = helper(idx+1, s, take, DP) | helper(idx+1, s, take+1, DP) | helper(idx+1, s, take-1, DP);
-        }  else {
-            if(s[idx] == '(')
-                cache = helper(idx+1, s, take+1, DP);
-            else cache = helper(idx+1, s, take-1, DP);
-        }
-        return cache;
-    }
-    
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<vector<int>> DP(n+1, vector<int>(n+1, -1));
-        return helper(0, s, 0, DP);
+        if(s.size() == 0) return true;
+        stack<int>s1, s2;
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(') {
+                s1.push(i);
+            }
+            else if(s[i] == '*') {
+                s2.push(i);
+            } 
+            else {
+                if(s1.size()>0) s1.pop();
+                else if(s2.size()>0) s2.pop();
+                else return false;
+            }
+        }
+        
+        while(!s1.empty() && !s2.empty()){
+            if(s1.top()>s2.top()) return false;
+            s1.pop();
+            s2.pop();
+        }
+        
+        if(s1.size()>0) return false;
+        
+        return true;
     }
 };

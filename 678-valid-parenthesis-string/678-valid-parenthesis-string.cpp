8c84d29488aaac189c4cb2fa46b8e57dc04ec3d2
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-21 18:28:13 +0600

    Time: 9 ms (11.30%), Space: 8.2 MB (11.08%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    bool helper(int idx, string& s, int take, vector<vector<int>>& DP){
+        if(take <0 || idx > s.size()){
+            return 0;
+        }
+        if(idx == s.size() && take == 0){
+            return 1;
+        }
+        
+        int& cache = DP[idx][take];
+        if(cache != -1){
+            return cache;
+        }
+        
+        if(s[idx] == '*'){
+           cache = helper(idx+1, s, take, DP) or helper(idx+1, s, take+1, DP) or helper(idx+1, s, take-1, DP);
+        }  else {
+            if(s[idx] == '(')
+                cache = helper(idx+1, s, take+1, DP);
+            else cache = helper(idx+1, s, take-1, DP);
+        }
+        return cache;
+    }
+    
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+        
+        vector<vector<int>> DP(n+1, vector<int>(n+1, -1));
+        return helper(0, s, 0, DP);
+    }
+};

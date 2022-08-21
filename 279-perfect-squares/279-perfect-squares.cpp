796d41ce96d48a386d709a1a3f9dffabee11fbe5
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-21 20:37:19 +0600

    Time: 949 ms (8.66%), Space: 11.8 MB (19.34%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,36 +1,33 @@
 class Solution {
-private:
-    int helper(int idx, int target, vector<int>& nums, vector<vector<int>>& DP){
-        if(target == 0){
-            return 0;
-        }
-        if(idx == nums.size()){
-            return INT_MAX/2;
-        }
-        
-        int& cache = DP[idx][target];
-        if(cache != -1){
-            return cache;
-        }
-        
-        int way1 = 0, way2 = INT_MAX;
-        way1 = helper(idx+1, target, nums, DP);
-        if(target>=nums[idx]){
-            way2 = 1 + helper(idx, target-nums[idx], nums, DP);
-        }
-        return cache = min(way1, way2);
-    }
 public:
     int numSquares(int n) {
-        if(n == 1) return 1;
-        vector<int>nums;
+        if(n == 1 || n==2) return n;
+        vector<int>coins;
         int i = 1;
         while(i*i<=n){
-            nums.push_back(i*i);
+            coins.push_back(i*i);
             i++;
         }
-        int idx = nums.size();
-        vector<vector<int>> DP(idx+1, vector<int>(n+1, -1));
-        return helper(0, n, nums, DP);
+        int amount = n;
+        n = coins.size();
+
+        vector<int>previous(amount+1);
+        vector<int>current(amount+1);
+        
+        for(int tar = 0; tar<=amount; tar++){
+            previous[tar] = (tar%coins[0] == 0) ? tar/coins[0] : INT_MAX/2;
+        }
+        
+        for(int idx=1; idx<n ;idx++){
+            for(int tar = 0; tar<=amount; tar++){
+                current[tar] = previous[tar];
+                if(coins[idx]<=tar){
+                    current[tar] = min(current[tar], 1 + current[tar-coins[idx]]);
+                }
+            }
+            previous = current;
+        }
+        
+        return previous[amount];
     }
 };

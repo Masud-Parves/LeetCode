215b73a71e2e982cb0f32d895bb16b2d2c52c075
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-23 21:39:35 +0600

    Time: 6 ms (30.18%), Space: 9.9 MB (6.35%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -15,11 +15,25 @@ public:
         if(root == NULL){
             return NULL;
         }
-        TreeNode* left = invertTree(root->left);
-        TreeNode* right = invertTree(root->right);
         
-        root->left = right;
-        root->right = left;
+        stack<TreeNode*> stack;
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode* node = stack.top();
+            stack.pop();
+            
+            TreeNode* tmp = node->left;
+            node->left = node->right;
+            node->right = tmp;
+            if(node->right != NULL){
+                stack.push(node->right);
+            }
+            if(node->left != NULL){
+                stack.push(node->left);
+            }
+        }
+        
+        
         return root;
     }
 };

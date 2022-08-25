e1bab633e5c0c7c7501d8b29c4b980e78faf595b
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-25 09:10:02 +0600

    Time: 203 ms (53.51%), Space: 90.2 MB (5.09%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -10,22 +10,31 @@
  * };
  */
 class Solution {
-private:
-    void countGoodNodes(TreeNode* node, int tillMax, int& result){
-        if(node == NULL) return;
-        
-        if(node->val >= tillMax){
-            result++;
-            tillMax = node->val;
-        }
-        
-        countGoodNodes(node->left, tillMax, result);
-        countGoodNodes(node->right, tillMax, result);
-    }
 public:
     int goodNodes(TreeNode* root) {
         int result = 0;
-        countGoodNodes(root, root->val, result);
+        
+        queue<pair<TreeNode*, int>>Q;
+        Q.push({root, root->val});
+        
+        while(!Q.empty()){
+            TreeNode* node = Q.front().first;
+            int tillMax = Q.front().second;
+            Q.pop();
+            
+            if(node->val>=tillMax){
+                result++;
+                tillMax = node->val;
+            }
+            
+            if(node->left != NULL){
+                Q.push({node->left, tillMax});
+            }
+            if(node->right != NULL){
+                Q.push({node->right, tillMax});
+            }
+        }
+        
         return result;
     }
 };

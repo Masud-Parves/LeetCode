f89bd3a9806fca33b63d0d396d1e77d60a1ef2c7
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-26 07:27:19 +0600

    Time: 54 ms (63.17%), Space: 28.3 MB (10.85%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,17 +1,36 @@
 class Solution {
-public:
-    int minSubArrayLen(int target, vector<int>& nums) {
+private:
+    bool window_sliding(vector<int>& nums, int window, int target){
+        int window_sum = 0;
         int n = nums.size();
         int l = 0, r = 0;
-        int windowSum = 0, window = INT_MAX;
-        while(r<n){
-            windowSum += nums[r];
-            while(windowSum>=target){
-                window = min(window, r-l+1);
-                windowSum -= nums[l];
-                l++;
-            }
+        while(r<window){
+            window_sum += nums[r];
             r++;
+        }
+        if(window_sum>=target) return true;
+        for(int r=window; r<n; r++){
+            window_sum += nums[r];
+            window_sum -= nums[l];
+            l++;
+            if(window_sum>=target) return true;
+            
+        }
+        return false;
+    }
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int window = INT_MAX;
+        int l = 0, r = nums.size();
+        
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(window_sliding(nums, mid, target)){
+                r = mid-1;
+                window = mid;
+            } else {
+                l = mid+1;
+            }
         }
         return window == INT_MAX ? 0 : window;
     }

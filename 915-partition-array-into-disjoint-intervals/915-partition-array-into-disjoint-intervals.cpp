b9ab880a8ae9de50fbc0a0aea039319757ad4d26
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-27 13:46:36 +0600

    Time: 187 ms (85.06%), Space: 94.2 MB (80.13%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
@@ -3,20 +3,18 @@ public:
     int partitionDisjoint(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> minRight(n);
         int currMax = nums[0];
-        minRight[n-1] = nums[n-1];
-        
-        for(int i=n-2; i>=0; i--){
-            minRight[i] = min(minRight[i+1], nums[i]);
-        }
+        int possibleMax = nums[0];
+        int length = 1;
         
         for(int i=1; i<n; i++){
-            if(currMax <= minRight[i]){
-                return i;
+            if(nums[i] < currMax){
+                length = i+1;
+                currMax = possibleMax;
+            } else {
+                possibleMax = max(possibleMax, nums[i]);
             }
-            currMax = max(currMax, nums[i]);
         }
-        return -1;
+        return length;
     }
 };

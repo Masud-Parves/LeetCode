3e5a4dcb79a6dce9d9f9054122e9d43ffeb78738
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-28 10:09:08 +0600

    Time: 60 ms (74.76%), Space: 19.1 MB (9.19%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    bool check(vector<int>& piles, int k, int h){
+        long int hours = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            hours += ceil((double) piles[i] / k);
+        }
+        return hours<=h;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        
+        int low = 1;
+        int high = 0;
+        for (int i = 0; i < n; i++) {
+            high = max(high, piles[i]);
+        }
+        
+        int result = high;
+        
+        while (low < high) {
+            int k = low + (high - low) / 2;
+
+            if (check(piles, k, h)) {
+                result = min(result, k);
+                high = k;
+            } else {
+                low = k + 1;
+            }
+        }
+        
+        return high;
+    }
+};

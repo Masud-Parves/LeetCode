f862d5d7ec64fa8d936ecca7d5df36c1b19b50da
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-28 11:02:12 +0600

    Time: 7 ms (53.92%), Space: 11.2 MB (29.59%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,45 +1,31 @@
 class Solution {
-private:
-    int findPivotElement(vector<int>& nums){
-        int start = 0, end = nums.size()-1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(mid+1<=end && nums[mid]>nums[mid+1]) return mid;
-            else if(mid-1>=start && nums[mid-1]>nums[mid]) return mid-1;
-            else if(nums[start]>nums[mid]) {
-                end = mid-1;
-            } else {
-                start = mid+1;
-            }
-        }
-        return -1;
-    }
-    
-    int binarySearch(vector<int>& nums, int target, int start, int end){
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid] == target){
-                return mid;
-            } else if(nums[mid]<target){
-                start = mid+1;
-            } else {
-                end = mid-1;
-            }
-        }
-        return -1;
-    }
-    
 public:
+    
+    
+    // 5 6 7 8 0 1 2 3 4
+    //              ^
     int search(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1;
-        int retIdx = -1;
-        int pivot = findPivotElement(nums);
-        if(pivot == -1) return binarySearch(nums, target, 0, end);
-        
-        if(nums[pivot] == target) return pivot;
-        
-        if(target>=nums[0]) return binarySearch(nums, target, start, pivot-1);
-        
-        return binarySearch(nums, target, pivot+1, end);
+        int n = nums.size();
+                 
+        int left = 0, right = n-1, mid;
+        while(left<=right){
+            mid = left + (right-left)/2;
+            if(nums[mid] == target) return mid;
+            
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<=nums[mid]){
+                    right = mid-1;
+                } else {
+                    left = mid+1;
+                }
+            } else {
+                if(target>=nums[mid] && target<=nums[right]){
+                    left = mid+1;
+                } else {
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
     }
 };

74e6930623fab629ea1ffc43e9f149c23dd66922
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-28 17:42:23 +0600

    Time: 47 ms (82.04%), Space: 89.3 MB (62.51%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -2,25 +2,38 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        
-        vector<int> mergedArray;
-        
-        int i=0, j=0;
-        while(i<n && j<m){
-            if(nums1[i]<nums2[j]){
-                mergedArray.push_back(nums1[i++]);
-            } else {
-                mergedArray.push_back(nums2[j++]);
-            }
+        int total = n+m;
+        if(n>m){
+        	return findMedianSortedArrays(nums2, nums1);
         }
-        
-        while(i<n) mergedArray.push_back(nums1[i++]);
-        while(j<m) mergedArray.push_back(nums2[j++]);
-        
-        int pivot = (m+n)/2;
-        if((m+n)%2 == 1){
-            return mergedArray[pivot];
-        } 
-        return (mergedArray[pivot] + mergedArray[pivot-1])/2.0;
+
+        int low = 0, high = n;
+        double result = 0.0;
+        while(low<=high){
+        	int pivot = low + (high-low)/2;
+
+        	int remain = (total+1)/2 - pivot;
+
+        	int left1 = (pivot>0) ? nums1[pivot-1] : INT_MIN;
+        	int right1 = (pivot<n) ? nums1[pivot] : INT_MAX; 
+
+        	int left2 = (remain>0) ? nums2[remain-1] : INT_MIN;
+        	int right2 = (remain<m) ? nums2[remain] : INT_MAX;
+
+        	if(left1<=right2 && left2<=right1){
+
+        		if(total%2==0){
+        			return (max(left1, left2) + min(right1, right2))/2.0;
+        		}
+        		return max(left1, left2);
+        	} else if(left1 > right2) {
+        		high = pivot - 1;
+        	} else {
+        		low = pivot + 1;
+        	}
+
+        }
+        return -1;
+
     }
 };

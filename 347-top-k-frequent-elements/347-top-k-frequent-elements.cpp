4a85c61f44db24dd15dce39e925f954c759b590a
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-28 23:32:23 +0600

    Time: 21 ms (71.09%), Space: 13.6 MB (89.50%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,24 @@
-// Approach 01: Hashmap, Priority Queue
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-  		int n = nums.size();
-  		unordered_map<int, int>freq;
-  		for(int i=0; i<n; i++){
-  			freq[nums[i]]++;
-  		}
-  		priority_queue<pair<int, int>> Q;
-      for(auto& f : freq){
-      	Q.push({f.second, f.first});
-      }
-      vector<int> result;
-      while(k>=1){
-      	int element = Q.top().second;
-        Q.pop();
-        result.push_back(element);
-      	k--;
-      }
-      return result;
+        unordered_map<int, int>freq;
+        
+        for(auto& num : nums){
+            freq[num]++;
+        }
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>Q;
+        
+        for(auto& f : freq){
+            Q.push({f.second, f.first});
+            if(Q.size()>k) Q.pop();
+        }
+        int idx = k-1;
+        vector<int>result(k);
+        while(!Q.empty()){
+            result[idx--] = Q.top().second;
+            Q.pop();
+        }
+        return result;
     }
 };

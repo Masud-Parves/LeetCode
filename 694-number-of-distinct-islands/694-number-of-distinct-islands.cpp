9b824b41a093b83587585f86d4898f3d5511c631
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-08-31 23:28:34 +0600

    Time: 80 ms (40.87%), Space: 33.8 MB (31.36%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
@@ -6,30 +6,23 @@ private:
     bool isValid(int r, int c, vector<vector<int>>& grid){
         return (r>=0 && r<grid.size() && c>=0 && c<grid[0].size() && grid[r][c] == 1);
     }
-    void bfs(int sr, int sc, vector<vector<int>>& grid, vector<vector<bool>>& visited, set<vector<pair<int, int>>>& unique){
-        queue<pair<int, int>> Q;
-        vector<pair<int, int>> shape;
-        visited[sr][sc] = true;
-        Q.push({sr, sc});
-        shape.push_back({sr-sr, sc-sc});
-        while(!Q.empty()){
-            int ur = Q.front().first;
-            int uc = Q.front().second;
-            Q.pop();
-            
-            for(int i=0; i<4; i++){
-                int vr = ur + fx[i];
-                int vc = uc + fy[i];
-                if(isValid(vr, vc, grid) && visited[vr][vc] == false){
-                    visited[vr][vc] = true;
-                    Q.push({vr, vc});
-                    shape.push_back({vr-sr, vc-sc});
-                }
-            }
+    
+    
+    void dfs(int r, int c, vector<vector<int>>& grid, int baseRow, int baseCol, vector<vector<bool>>& visited, vector<pair<int, int>>& shape){
+        if(r<0 || r==grid.size() || c<0 || c == grid[0].size() || grid[r][c] == 0 || visited[r][c] == true){
+            return;
         }
-        unique.insert(shape);
+        
+        visited[r][c] = true;
+        shape.push_back({r-baseRow, c-baseCol});
+        for(int i=0; i<4; i++){
+            int vr = r + fx[i];
+            int vc = c + fy[i];
+            dfs(vr, vc, grid, baseRow, baseCol, visited, shape);
+        }
         return;
     }
+    
 public:
     int numDistinctIslands(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
@@ -39,7 +32,9 @@ public:
         for(int r=0; r<rows; r++){
             for(int c=0; c<cols; c++){
                 if(grid[r][c] == 1 && visited[r][c] == false){
-                    bfs(r, c, grid, visited, unique);
+                    vector<pair<int, int>> shape;
+                    dfs(r, c, grid, r, c, visited, shape);
+                    unique.insert(shape);
                 }
             }
         }

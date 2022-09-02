9022ccfbee2efd848fb1f9623ee51bff76d887d4
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-09-02 08:45:28 +0600

    Time: 236 ms (18.39%), Space: 89.3 MB (7.25%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int helper(int currIdx, int dest, int isBackPossible, int forword, int backword, unordered_map<int, int>& seen, vector<vector<int>>& DP){
+        if(currIdx == dest){
+            return 0;
+        }
+        
+        if(currIdx<0 || currIdx>6000 || seen.find(currIdx)!=seen.end()){
+            return 1e9;
+        }
+        int& cache = DP[currIdx][isBackPossible];
+        if(cache != -1){
+            return cache;
+        }
+        
+        cache = 1 + helper(currIdx+forword, dest, 1, forword, backword, seen, DP);
+        
+        if(isBackPossible == 1){
+            cache = min(cache, 1 + helper(currIdx-backword, dest, 0, forword, backword, seen, DP));
+        }
+        return cache;
+    }
+public:
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        unordered_map<int, int> seen;
+        for(auto& val : forbidden){
+            seen[val]++;
+        }
+        vector<vector<int>> DP(7001, vector<int>(2, -1));
+        int result =helper(0, x, 1, a, b, seen, DP);
+        return (result >= 1e9) ? -1 : result;
+    }
+};

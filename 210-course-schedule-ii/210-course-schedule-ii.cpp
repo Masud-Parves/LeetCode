ef8a7da6790c8c2c719a2068ad75503e5ff3278f
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-09-06 07:14:17 +0600

    Time: 37 ms (46.80%), Space: 13.9 MB (60.48%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,46 +1,32 @@
 class Solution {
-private:
-    bool dfs(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& path, stack<int>& topo){
-        
-        visited[node] = path[node] = true;
-        
-        for(auto& vNode : graph[node]){
-            if(visited[vNode] == false){
-                if(dfs(vNode, graph, visited, path, topo) == false){
-                    return false;
-                }
-            } else if(path[vNode] == true) {
-                return false;
-            }
-        }
-        path[node] = false;
-        topo.push(node);
-        return true;
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        for(auto& course : prerequisites){
-            graph[course[1]].push_back(course[0]);
+        vector<int>ans;
+        vector<int>graph[numCourses];
+        vector<int>inDegree(numCourses, 0);
+        
+        
+        for(auto pre : prerequisites){
+            graph[pre[1]].push_back(pre[0]);
+            inDegree[pre[0]]++;
+        }
+        queue<int>Q;
+        for(int i=0; i<numCourses ; i++){
+            if(inDegree[i] == 0) Q.push(i);
         }
         
-        vector<bool> visited(numCourses, false);
-        vector<bool> path(numCourses, false);
-        stack<int> topo;
-        
-        for(int i=0; i<numCourses; i++){
-            if(visited[i] == false){
-                if(dfs(i, graph, visited, path, topo) == false){
-                    return vector<int>();
-                }
+        while(!Q.empty()){
+            int u = Q.front();
+            Q.pop();
+            ans.push_back(u);
+            for(int v:graph[u]){
+                inDegree[v]--;
+                if(inDegree[v] == 0) Q.push(v);
             }
         }
         
-        vector<int> result;
-        while(!topo.empty()){
-            result.push_back(topo.top());
-            topo.pop();
-        }
-        return result.size() == numCourses ? result : vector<int>();
+        vector<int> ret;
+        
+        return ans.size() == numCourses ? ans : ret;
     }
 };

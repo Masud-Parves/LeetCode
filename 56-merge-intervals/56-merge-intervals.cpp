511d047c3b88b6c112c578dc9ad877cd491490ee
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-09-11 14:17:05 +0600

    Time: 62 ms (51.65%), Space: 19.1 MB (44.58%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,21 @@
+bool compare(vector<int>& a, vector<int>& b){
+    return a[0]<b[0] || (a[0]==b[0] && a[1]<b[1]);
+}
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+
+        sort(intervals.begin(), intervals.end());
+        result.push_back(intervals[0]);
+        
+        for(int i=1; i<intervals.size(); i++){
+            if(result.back()[1]>=intervals[i][0]){
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};

9effbc48de8a52262626c70d68266b474a72e6b1
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-09-18 22:34:12 +0600

    Time: 58 ms (18.42%), Space: 13.6 MB (98.24%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,33 +1,63 @@
-class Solution {
-private:
-    void bfs(int src, vector<vector<int>>& mat, vector<bool>& visited){
-        queue<int>Q;
-        visited[src] = true;
-        Q.push(src);
+class Union_Find{
+public:
+    Union_Find(int sz) : root(sz), rank(sz), count(sz){
+        for(int i=0; i<sz; i++){
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+    
+    int Find(int x){
+        if(x == root[x]){
+            return x;
+        }
+        return root[x] = Find(root[x]);
+    }
+    
+    void Union(int x, int y){
+        int rootX = Find(x);
+        int rootY = Find(y);
         
-        while(!Q.empty()){
-            int u = Q.front();
-            Q.pop();
-            
-            for(int i=0; i<mat.size(); i++){
-                if(mat[u][i] == 1 && visited[i] == false){
-                    Q.push(i);
-                    visited[i] = true;
+        if(rootX != rootY){
+            if(rank[rootX]>rank[rootY]){
+                root[rootY] = rootX;
+            }else if(rank[rootX] < rank[rootY]){
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            count--;
+        }
+    }
+    
+    int getCount(){
+        return count;
+    }
+    
+private:
+    vector<int>root;
+    vector<int>rank;
+    int count;
+};
+
+
+class Solution {
+public:
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int N = isConnected.size();
+        if(N==0) return 0;
+        
+        Union_Find dsu(N);
+        
+        for(int i=0; i<N ; i++){
+            for(int j=1; j<N ; j++){
+                if(isConnected[i][j] == 1){
+                    dsu.Union(i, j);
                 }
             }
         }
-    }
-public:
-    int findCircleNum(vector<vector<int>>& mat) {
-        int n = mat.size();
-        vector<bool> visited(n, false);
-        int ret = 0;
-        for(int i=0; i<n; i++){
-            if(visited[i] == false){
-                bfs(i, mat, visited);
-                ret++;
-            }
-        }
-        return ret;
+        return dsu.getCount();
     }
 };

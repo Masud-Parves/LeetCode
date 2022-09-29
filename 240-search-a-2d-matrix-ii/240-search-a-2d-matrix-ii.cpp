1b5fa24a55818f9d71c8826041f0d2016a55db04
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-09-30 01:04:42 +0600

    Time: 271 ms (24.54%), Space: 14.9 MB (21.67%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,52 +1,20 @@
 class Solution {
-private:
-    bool binarySearch(vector<vector<int>>& matrix, int target, int start, bool isVertical){
-        int rows = matrix.size(), cols = matrix[0].size();
-        
-        int lo = start, hi = isVertical == true ? cols-1 : rows-1;
-        
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(isVertical){
-                if(matrix[start][mid] == target){
-                    return true;
-                }
-                if(matrix[start][mid]<target){
-                    lo = mid+1;
-                } else {
-                    hi = mid-1;
-                }
-            } else {
-                if(matrix[mid][start] == target){
-                    return true;
-                }
-                if(matrix[mid][start]<target){
-                    lo = mid+1;
-                } else {
-                    hi = mid-1;
-                }
-            }
-        }
-        return false;
-    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size(), cols = matrix[0].size();
-        if(rows == 1 && cols == 1){
-            return matrix[0][0] == target;
-        }
-        int minDiagonal = min(rows, cols);
-        for(int i=0; i<minDiagonal ; i++){
-            
-            bool vertical = binarySearch(matrix, target, i, true);
-            bool horizontal = binarySearch(matrix, target, i, false);
-            
-            if(vertical || horizontal){
+        
+        int r = 0, c = cols-1;
+        while(r<rows && c>= 0){
+            if(matrix[r][c] == target){
                 return true;
             }
             
+            if(matrix[r][c] < target){
+                r++;
+            } else {
+                c--;
+            }
         }
-        
         return false;
     }
 };

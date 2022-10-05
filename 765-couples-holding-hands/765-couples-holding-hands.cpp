bca2f79f8994dd1a5209fde6124d0f1ffb117471
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-05 19:42:42 +0600

    Time: 5 ms (29.49%), Space: 7.3 MB (96.72%) - LeetHub

---
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    int find(int val, vector<int>& row){
+        if(val%2==0){
+            for(int i=0; i<row.size(); i++){
+                if(val+1 == row[i]){
+                    return i;
+                }
+            }
+        } else {
+            for(int i=0; i<row.size(); i++){
+                if(val-1 == row[i]){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+public:
+    int minSwapsCouples(vector<int>& row) {
+        int n = row.size();
+        int ret = 0;
+
+        for(int i=0; i<n; i+=2){
+            int val = row[i];
+            if(val%2==0){
+                // odd ======> 3, find 2
+                if(val+1 == row[i+1]) continue;
+                else {
+                    int idx = find(val, row);
+                    swap(row[i+1], row[idx]);
+                    ret++;
+                }
+            } else {
+                if(val-1 == row[i+1]) continue;
+                else {
+                    int idx = find(val, row);
+                    swap(row[i+1], row[idx]);
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+};

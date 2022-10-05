8848bd3d9c0b374839b7171369122d406d4d8b2c
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-05 20:05:03 +0600

    Time: 11 ms (6.92%), Space: 7.6 MB (26.58%) - LeetHub

--- 765-couples-holding-hands/765-couples-holding-hands.cpp
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -22,21 +22,34 @@ public:
         int n = row.size();
         int ret = 0;
 
+        unordered_map<int, int> seen;
+        for(int i=0; i<n; i++){
+            seen[row[i]] = i;
+        }
+        
         for(int i=0; i<n; i+=2){
             int val = row[i];
             if(val%2==0){
                 // odd ======> 3, find 2
                 if(val+1 == row[i+1]) continue;
                 else {
-                    int idx = find(val, row);
+                    int idx = seen[val+1];
+                    cout << idx << " " << find(val, row) << endl;
+                    seen[row[i+1]] = idx;
+                    seen[row[idx]] = i+1;
                     swap(row[i+1], row[idx]);
+                   
                     ret++;
                 }
             } else {
                 if(val-1 == row[i+1]) continue;
                 else {
-                    int idx = find(val, row);
+                    int idx = seen[val-1];
+                    cout << idx << " " << find(val, row) << endl;
+                    seen[row[i+1]] = idx;
+                    seen[row[idx]] = i+1;
                     swap(row[i+1], row[idx]);
+
                     ret++;
                 }
             }

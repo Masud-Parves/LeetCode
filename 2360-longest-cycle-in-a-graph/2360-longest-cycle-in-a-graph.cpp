5def34c9782be7fc62d46506ea0938c6d85e6f24
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-06 03:05:57 +0600

    Time: 1080 ms (18.44%), Space: 173.4 MB (33.85%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,57 +1,50 @@
 class Solution {
 private:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& colors, vector<int>& cycle, int& cycledNode){
-
-	colors[node] = 1;
-	cycle.push_back(node);
-
-	for(int vNode : graph[node]){
-		if(colors[vNode] == -1){
-			if(dfs(vNode, graph, colors, cycle, cycledNode) == true){
-				return true;
-			} 
-		}else if(colors[vNode] == 1){
-			cycledNode = vNode;
-			return true;
-		}
-	}
-
-	cycle.pop_back();
-	colors[node] = 2;
-
-	return false;
-}
+   bool dfs(int uNode, vector<vector<int>>& graph, vector<int>& colors){
+       colors[uNode] = 1;
+       
+       for(int vNode : graph[uNode]){
+           if(colors[vNode] == -1){
+               if(dfs(vNode, graph, colors) == true){
+                   return true;
+               }
+           } else if(colors[vNode] == 1){
+               return true;
+           }
+       }
+       
+       colors[uNode] = 2;
+       return false;
+   }
 public:
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
         vector<vector<int>>graph(n);
         for(int i=0; i<n; i++){
             if(edges[i] == -1) continue;
-            graph[i].push_back(edges[i]);
+            graph[edges[i]].push_back(i);
         }
         
         vector<int> colors(n, -1);
-        vector<int> cycleNodes;
         int longestCycle = -1;
         for(int i=0; i<n ; i++){
+            bool isCycle = false;
             if(colors[i] == -1){
-                //cout << i << endl;
-                cycleNodes.clear();
-                
-                int startNode = -1;
-                dfs(i, graph, colors, cycleNodes, startNode);
-                if(startNode==-1) continue;
-                int lengthOfCycle = 0;
-                //cout << "start : " << startNode << endl;
-                for(auto& c : cycleNodes){
-                    if(c == startNode) startNode = -1;
-                    if(startNode == -1){
-                        lengthOfCycle++;
-                    }
-                }
-                longestCycle = max(longestCycle, lengthOfCycle);
-                cout << endl;
+                isCycle = dfs(i, graph, colors);
             }
+            int currCycleLength = 0;
+            if(isCycle){
+                
+                int node = i;
+                currCycleLength++;
+                while(i != edges[node]){
+                    currCycleLength++;
+                    node = edges[node];
+                }
+                
+                
+            }
+            longestCycle = max(longestCycle, currCycleLength);
         }
         return longestCycle==0? -1 : longestCycle;
     }

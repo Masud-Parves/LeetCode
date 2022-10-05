280b07334895688bc3f83dcca8a9443e8bec7e42
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-05 20:16:19 +0600

    Time: 2 ms (68.69%), Space: 7.4 MB (53.40%) - LeetHub

--- 765-couples-holding-hands/765-couples-holding-hands.cpp
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -1,41 +1,34 @@
 class Solution {
+private:
+    vector<int> root;
+    int find(int x){
+        if(root[x] == x) return x;
+        return root[x] = find(root[x]);
+    }
+    
+    bool make_union(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX == rootY) return false;
+        root[rootX] = rootY;
+        return true;
+    }
+    
 public:
     int minSwapsCouples(vector<int>& row) {
         int n = row.size();
-        int ret = 0;
-
-        unordered_map<int, int> seen;
-        for(int i=0; i<n; i++){
-            seen[row[i]] = i;
-        }
+        root = vector<int>(n);
         
         for(int i=0; i<n; i+=2){
-            int val = row[i];
-            if(val%2==0){
-                // odd ======> 3, find 2
-                if(val+1 == row[i+1]) continue;
-                else {
-                    int idx = seen[val+1];
-                    
-                    seen[row[i+1]] = idx;
-                    seen[row[idx]] = i+1;
-                    swap(row[i+1], row[idx]);
-                   
-                    ret++;
-                }
-            } else {
-                if(val-1 == row[i+1]) continue;
-                else {
-                    int idx = seen[val-1];
-                    
-                    seen[row[i+1]] = idx;
-                    seen[row[idx]] = i+1;
-                    swap(row[i+1], row[idx]);
-
-                    ret++;
-                }
+            root[row[i]] = row[i];
+            root[row[i+1]] = row[i];
+        }
+        int count = 0;
+        for(int i=0; i<n; i+=2){
+            if(make_union(i, i+1)){
+                count++;
             }
         }
-        return ret;
+        return count;
     }
 };

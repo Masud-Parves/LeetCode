6b9e70e272704b601ca764992277886157c84701
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-09 01:14:08 +0600

    Time: 155 ms (19.62%), Space: 55.4 MB (64.20%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int longestOnes(vector<int>& arr, int k) {
         int n = arr.size();
-        unordered_map<int, int> seen;
         
         int l = 0, currMax = 0;
-        
+        int countZero = 0;
         for(int r=0; r<n; r++){
             if(arr[r]==0){
-                seen[arr[r]]++;
-                if(seen[arr[r]]>k){
-                    while(seen[arr[r]]>k) {
-                        seen[arr[l]]--;
-                        l++;
-                    }
+                countZero++;
+                while(countZero>k) {
+                    if(arr[l]==0)
+                        countZero--;
+                    l++;
                 }
+
             }
             if(r-l+1>currMax){
                 currMax = max(currMax, r-l+1);

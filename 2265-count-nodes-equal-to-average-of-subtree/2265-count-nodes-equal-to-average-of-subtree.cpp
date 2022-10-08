0759ee5266567a694bfad8e9ae67bd128b3e4eaf
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-09 03:21:06 +0600

    Time: 19 ms (49.53%), Space: 11.9 MB (86.87%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -13,44 +13,36 @@ class Solution {
 private:
     unordered_map<TreeNode*, pair<int, int>> cache;
     
-    pair<int, int> dfs(TreeNode* node){
+    pair<int, int> dfs(TreeNode* node, int& answer){
         if(node == NULL){
                 // sum, count
             return {0, 0};
         }
         
-        auto left = dfs(node->left);
-        auto right = dfs(node->right);
-        
-        pair<int, int> currNode = {left.first+right.first+node->val, left.second+right.second+1};
-        
-        return cache[node] = currNode;
-    }
-    
-    void preorderTraversal(TreeNode* node, int& answer){
-        if(node == NULL) {
-            return;
-        }
         if(node->left == NULL && node->right == NULL){
             answer++;
-            return;
+            return {node->val, 1};
         }
         
-        int sum = cache[node].first, child = cache[node].second;
-        int avg = sum/child;
+        auto left = dfs(node->left, answer);
+        auto right = dfs(node->right, answer);
+        
+        int sum = left.first+right.first+node->val, count = left.second+right.second+1, avg=0;
+        avg = sum/count;
         if(avg == node->val){
             answer++;
         }
         
-        preorderTraversal(node->left, answer);
-        preorderTraversal(node->right, answer);
+        return {left.first+right.first+node->val, count = left.second+right.second+1};
     }
     
 public:
     int averageOfSubtree(TreeNode* root) {
-        dfs(root);
+        if(root == NULL){
+            return 0;
+        }
         int result = 0;
-        preorderTraversal(root, result);
+        dfs(root, result);
         return result;
     }
 };

58c564900ad0b3b525205899844436a0b06146e1
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-09 01:40:52 +0600

    Time: 83 ms (50.13%), Space: 8.6 MB (29.72%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         
         int l=0, ans = 0;
-        unordered_map<char, int> seen;
+        unordered_map<char, int> seen; // O(1), because only used for 3 characters
         for(int r=0; r<n; r++){
             seen[s[r]]++;
             while(seen['a']>0 && seen['b']>0 && seen['c']>0){

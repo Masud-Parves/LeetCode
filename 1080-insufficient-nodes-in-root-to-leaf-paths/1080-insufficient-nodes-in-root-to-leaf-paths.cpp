67d5cdd0f5bf3dad22e90ecb7e11f601c6982126
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-09 11:11:51 +0600

    Time: 88 ms (44.40%), Space: 33.1 MB (61.20%) - LeetHub

---
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* dfsTraversal(TreeNode* node, int& limit, int sum){
+        if(node == NULL){
+            return NULL;
+        }
+        
+        sum+= node->val;
+        if(node->left == NULL && node->right == NULL){
+            if(sum<limit) return NULL;
+            return node;
+        }
+        
+        TreeNode* leftNode = dfsTraversal(node->left, limit, sum);
+        TreeNode* rightNode = dfsTraversal(node->right, limit, sum);
+        if(leftNode == NULL){
+            node->left = NULL;
+        }
+        if(rightNode == NULL){
+            node->right = NULL;
+        }
+        
+        if(leftNode == NULL && rightNode == NULL){
+            return NULL;
+        }
+        return node;
+    }
+public:
+    TreeNode* sufficientSubset(TreeNode* root, int limit) {
+        if(root == NULL){
+            return NULL;
+        }
+        int sum = 0;
+        TreeNode* newRoot = dfsTraversal(root, limit, sum);
+        return newRoot;
+    }
+};

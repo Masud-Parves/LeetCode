86d2e8510c3287032fa2b24f133b6b43d34f222d
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-10 00:47:39 +0600

    Time: 41 ms (52.24%), Space: 8.4 MB (62.31%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,16 +3,17 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
         sort(satisfaction.begin(), satisfaction.end());
-        vector<vector<int>> cache(n+1, vector<int>(n+1, 0));
+        vector<int>nextRow(n+1, 0), currentRow(n+1, 0);
         
         for(int i=n-1; i>=0; i--){
             for(int t=i; t>=0; t--){
-                int way1 = cache[i+1][t];
-                int way2 = (t+1)*satisfaction[i] + cache[i+1][t+1];
-                cache[i][t] = max(way1, way2);
+                int way1 = nextRow[t];
+                int way2 = (t+1)*satisfaction[i] + nextRow[t+1];
+                currentRow[t] = max(way1, way2);
             }
+            nextRow = currentRow;
         }
 
-        return cache[0][0];
+        return nextRow[0];
     }
 };

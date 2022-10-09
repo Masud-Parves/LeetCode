48169a4f9689a840cdd1aa7ae62cf18e5d7758c4
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-10 00:45:23 +0600

    Time: 35 ms (56.68%), Space: 21 MB (20.77%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,24 +1,18 @@
 class Solution {
-private:
-    int helper(int idx, vector<int>& satisfaction, int time, vector<vector<int>>& cache){
-        if(idx == satisfaction.size()){
-            return 0;
-        }
-        
-        int& currState = cache[idx][time];
-        if(currState != -1){
-            return currState;
-        }
-        
-        int way1 = helper(idx+1, satisfaction, time, cache);
-        int way2 = time*satisfaction[idx] + helper(idx+1, satisfaction, time+1, cache);
-        return currState = max(way1, way2);
-    }
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
         sort(satisfaction.begin(), satisfaction.end());
-        vector<vector<int>> cache(n, vector<int>(n+1, -1));
-        return helper(0, satisfaction, 1, cache);
+        vector<vector<int>> cache(n+1, vector<int>(n+1, 0));
+        
+        for(int i=n-1; i>=0; i--){
+            for(int t=i; t>=0; t--){
+                int way1 = cache[i+1][t];
+                int way2 = (t+1)*satisfaction[i] + cache[i+1][t+1];
+                cache[i][t] = max(way1, way2);
+            }
+        }
+
+        return cache[0][0];
     }
 };

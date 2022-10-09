ccda056f6352bd79980cd984b8b10194b2d5db24
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-09 22:51:53 +0600

    Time: 157 ms (60.33%), Space: 27.9 MB (84.42%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
@@ -11,31 +11,40 @@
  */
 class Solution {
 private:
-    vector<TreeNode*> helper(int n){
+    vector<TreeNode*> helper(int n, unordered_map<int, vector<TreeNode*>>& cache){
         vector<TreeNode*> result;
         if(n==1){
             TreeNode* baseRoot = new TreeNode(0);
             result.push_back(baseRoot);
             return result;
-        } else if(n%2==1){
+        }
+        
+        if(cache.find(n) != cache.end()){
+            return cache[n];
+        }
+        
+        if(n%2==1){
             for(int r=1; r<=n; r++){
-                vector<TreeNode*> left = helper(r-1);
-                vector<TreeNode*> right = helper(n-r);
-                for(auto& leftNode : left){
-                    for(auto& rightNode : right){
+                vector<TreeNode*> left = helper(r-1, cache);
+                vector<TreeNode*> right = helper(n-r, cache);
+                
+                for(auto& leftFBT : left){
+                    for(auto& rightFBT : right){
                         TreeNode* root = new TreeNode(0);
-                        root->left = leftNode;
-                        root->right = rightNode;
+                        root->left = leftFBT;
+                        root->right = rightFBT;
                         result.push_back(root);
                     }
                 }
             }
         }
-        return result;
+        
+        return cache[n] = result;
     }
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
-        vector<TreeNode*> result = helper(n);
+        unordered_map<int, vector<TreeNode*>> cache;
+        vector<TreeNode*> result = helper(n, cache);
         return result;
     }
 };

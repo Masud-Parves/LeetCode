e406e212e1e498efc5c36a80315a18d41f566724
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-10 12:20:13 +0600

    Time: 112 ms (26.06%), Space: 22.4 MB (34.69%) - LeetHub

--- 554-brick-wall/554-brick-wall.cpp
+++ 554-brick-wall/554-brick-wall.cpp
@@ -8,18 +8,15 @@ public:
         }
         
         unordered_map<long long, int> cache;
-
+        int maxEmpty = 0;
         for(int r=0; r<rows ; r++){
             int prev = 0;
             for(int c=0; c<wall[r].size()-1; c++){
                 int currVal = prev + wall[r][c];
                 cache[currVal]++;
+                maxEmpty = max(maxEmpty, cache[currVal]);
                 prev = currVal;
             }
-        }
-        int maxEmpty = 0;
-        for(auto& m : cache){
-            maxEmpty = max(maxEmpty, m.second);
         }
         
         return rows-maxEmpty;

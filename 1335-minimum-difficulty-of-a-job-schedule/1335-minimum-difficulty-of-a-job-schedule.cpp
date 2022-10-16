2c45bbb00e2a0768635d812ef9d86d91592f7f3a
Pervej Mia <38131751+Masud-Parves@users.noreply.github.com>
2022-10-16 13:17:09 +0600

    Time: 134 ms (34.50%), Space: 7.9 MB (44.90%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,35 +1,34 @@
 class Solution {
 private:
-    
     int helper(int idx, int day, vector<int>& jobs, vector<vector<int>>& DP){
         int n = jobs.size();
         if(day == 1){
             int maxVal = 0;
-            for(int i = idx ; i<n; i++){
-                maxVal = max(jobs[i], maxVal);
+            for(int i=idx; i<n; i++){
+                maxVal = max(maxVal, jobs[i]);
             }
             return maxVal;
         }
-        
         
         int& cache = DP[idx][day];
         if(cache != -1){
             return cache;
         }
-        
-        int ans = INT_MAX, maxVal = 0;
-        
+        int maxVal = 0, maxProfit = INT_MAX;
         for(int i=idx; i<=n-day; i++){
             maxVal = max(maxVal, jobs[i]);
             int ret = helper(i+1, day-1, jobs, DP);
-            ans = min(ans, ret+maxVal);
+            maxProfit = min(maxProfit, ret + maxVal);
+            
         }
-        return cache = ans;
+        return cache = maxProfit;
     }
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int jobs = jobDifficulty.size();
-        if(jobs<d) return -1;
+        if(jobs<d) {
+            return -1;
+        }
         vector<vector<int>> DP(jobs+1, vector<int>(d+1, -1));
         return helper(0, d, jobDifficulty, DP);
     }
